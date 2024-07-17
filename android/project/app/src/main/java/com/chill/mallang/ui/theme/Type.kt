b0b78e0a47992bcfd3d7ad3a79d60a05f18ae626
이재한 <99114456+jaehan4707@users.noreply.github.com>
2024-07-17 10:53:10 +0900

    ui(#S11P12D203-38): FontStyle 적용

--- android/project/app/src/main/java/com/chill/mallang/ui/theme/Type.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/theme/Type.kt
@@ -2,33 +2,104 @@ package com.chill.mallang.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import com.chill.mallang.R
 
-// Set of Material typography styles to start with
+val galmuri11Family = FontFamily(
+    Font(R.font.galmuri11, FontWeight.Normal),
+    Font(R.font.galmuri11, FontWeight.SemiBold)
+)
+
+val omuyPrettyFamily = FontFamily(
+    Font(R.font.omyu_pretty, FontWeight.Normal),
+    Font(R.font.omyu_pretty, FontWeight.SemiBold),
+)
+
+private val galmuri11Headline1 = TextStyle(
+    fontFamily = galmuri11Family,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 28.sp,
+    letterSpacing = (-0.007).em
+)
+
+private val galmuri11Headline2 = TextStyle(
+    fontFamily = galmuri11Family,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 24.sp,
+    letterSpacing = (-0.007).em
+)
+
+private val galmuri11Body1 = TextStyle(
+    fontFamily = galmuri11Family,
+    fontWeight = FontWeight.Medium,
+    fontSize = 18.sp,
+    letterSpacing = (-0.007).em
+)
+
+private val galmuri11Body2 = TextStyle(
+    fontFamily = galmuri11Family,
+    fontWeight = FontWeight.Medium,
+    fontSize = 16.sp,
+    letterSpacing = (-0.007).em
+)
+
+private val galmuri11Body3 = TextStyle(
+    fontFamily = omuyPrettyFamily,
+    fontWeight = FontWeight.Medium,
+    fontSize = 14.sp,
+    letterSpacing = (-0.007).em
+)
+
+private val omyuPrettyHeadline1 = TextStyle(
+    fontFamily = omuyPrettyFamily,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 28.sp,
+    letterSpacing = (-0.007).em
+)
+
+private val omyuPrettyHeadline2 = TextStyle(
+    fontFamily = omuyPrettyFamily,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 24.sp,
+    letterSpacing = (-0.007).em
+)
+
+
+private val omyuPrettyBody1 = TextStyle(
+    fontFamily = omuyPrettyFamily,
+    fontWeight = FontWeight.Medium,
+    fontSize = 18.sp,
+    letterSpacing = (-0.007).em
+)
+
+private val omyuPrettyBody2 = TextStyle(
+    fontFamily = omuyPrettyFamily,
+    fontWeight = FontWeight.Medium,
+    fontSize = 16.sp,
+    letterSpacing = (-0.007).em
+)
+
+private val omyuPrettyBody3 = TextStyle(
+    fontFamily = omuyPrettyFamily,
+    fontWeight = FontWeight.Medium,
+    fontSize = 14.sp,
+    letterSpacing = (-0.007).em
+)
+
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    titleLarge = galmuri11Headline1,
+    titleMedium = galmuri11Headline2,
+    headlineLarge = omyuPrettyHeadline1,
+    headlineMedium = omyuPrettyHeadline2,
+    bodyLarge = galmuri11Body1,
+    bodyMedium = galmuri11Body2,
+    bodySmall = galmuri11Body3,
+    displayLarge = omyuPrettyBody1,
+    displayMedium = omyuPrettyBody2,
+    displaySmall = omyuPrettyBody3,
 )
