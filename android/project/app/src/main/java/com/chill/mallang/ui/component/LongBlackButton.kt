4e9f3d490620d52e66e82cff4286d9ecd9234177
NAKYUNG LEE <lnk12888@naver.com>
2024-07-18 10:58:40 +0900

    ui(#S11P12D203-21): LongBlackButton 컴포넌트 추가

---
+++ android/project/app/src/main/java/com/chill/mallang/ui/component/LongBlackButton.kt
@@ -0,0 +1,40 @@
+package com.chill.mallang.ui.component
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.unit.dp
+import com.chill.mallang.ui.theme.Gray4
+import com.chill.mallang.ui.theme.Gray6
+import com.chill.mallang.ui.theme.Typography
+
+@Composable
+fun LongBlackButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    text: String) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            contentColor = White,
+            containerColor = Gray6
+        ),
+        shape = RoundedCornerShape(10.dp),
+        border = BorderStroke(1.dp, Gray4),
+        modifier = modifier
+            .fillMaxWidth(0.8f)
+            .height(48.dp)
+    ) {
+        Text(
+            text,
+            style = Typography.displayLarge
+        )
+    }
+}
