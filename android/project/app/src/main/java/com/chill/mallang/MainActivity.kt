b1276f39dcf701c4e09b07d48bde2634b0ca4450
이재한 <99114456+jaehan4707@users.noreply.github.com>
2024-07-19 16:35:36 +0900

    feat(#S11P12D203-23): MainActivity MallangNavHost 추가

--- android/project/app/src/main/java/com/chill/mallang/MainActivity.kt
+++ android/project/app/src/main/java/com/chill/mallang/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.chill.mallang
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.navigation.compose.NavHost
 import com.chill.mallang.ui.feature.login.LoginScreen
 import com.chill.mallang.ui.feature.login.LoginViewModel
+import com.chill.mallang.ui.navigation.MallangNavHost
 import com.chill.mallang.ui.theme.MallangTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val loginViewModel: LoginViewModel by viewModels()
 
@@ -18,13 +23,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MallangTheme {
-                LoginScreen(
-                    viewModel = loginViewModel,
-                    onLoginClick = {
-                        // 로그인 성공 후 처리
-                        Toast.makeText(applicationContext, "로그인 성공", Toast.LENGTH_SHORT).show()
-                    }
-                )
+                MallangNavHost()
             }
         }
     }
