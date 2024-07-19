ef3e7db7265e507d828104af2571485755885291
Iantoo2 <rlawjddla0203@gmail.com>
2024-07-19 10:58:22 +0900

    Feat : Google OAuth Login Template Fixes S11P12D203-55 #73

---
+++ backend/mallang/src/main/java/com/chill/mallang/domain/user/controller/v1/UserController.java
@@ -0,0 +1,30 @@
+package com.chill.mallang.domain.user.controller.v1;
+
+import com.chill.mallang.domain.user.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/user")
+@Tag(name = "User API", description = "사용자 관련 API")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/home")
+    public String home(@AuthenticationPrincipal OAuth2User principal, Model model) {
+        model.addAttribute("name", principal.getAttribute("name"));
+        return "home";
+    }
+}
--- backend/mallang/src/main/java/com/chill/mallang/scheduler/QuizScheduler.java
+++ backend/mallang/src/main/java/com/chill/mallang/scheduler/QuizScheduler.java
@@ -20,11 +20,11 @@ public class QuizScheduler {
 
 
     // 문제 생성 스케쥴러
-    @Scheduled(fixedRate = 100_000)
-    public void generateQuiz(){
-        logger.info("Start generate quiz");
-        quizService.createQuizFromPrompt();
-        logger.info("Generate Quiz");
-    }
+//    @Scheduled(fixedRate = 100_000)
+//    public void generateQuiz(){
+//        logger.info("Start generate quiz");
+//        quizService.createQuizFromPrompt();
+//        logger.info("Generate Quiz");
+//    }
 
 }
