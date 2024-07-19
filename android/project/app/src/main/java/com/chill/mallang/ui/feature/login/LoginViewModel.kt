ab21de5d9086a89a496f5fe7054a8d057a9b30ff
NAKYUNG LEE <lnk12888@naver.com>
2024-07-19 10:00:26 +0900

    feat(S11P12D203-53): Firebase Authentication 로직 추가

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/login/LoginViewModel.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/login/LoginViewModel.kt
@@ -2,16 +2,29 @@ package com.chill.mallang.ui.feature.login
 
 import androidx.lifecycle.ViewModel
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-class LoginViewModel: ViewModel() {
+class LoginViewModel : ViewModel() {
     private val _loginResult = MutableStateFlow<Result<GoogleSignInAccount>?>(null)
     val loginResult: StateFlow<Result<GoogleSignInAccount>?> = _loginResult
 
+    private val auth = FirebaseAuth.getInstance()
+
     fun loginWithGoogle(account: GoogleSignInAccount?) {
-        account?.let {
-            _loginResult.value = Result.success(it)
+        account?.let { googleAccount ->
+            val credential = GoogleAuthProvider.getCredential(googleAccount.idToken, null)
+            auth.signInWithCredential(credential)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        _loginResult.value = Result.success(googleAccount)
+                    } else {
+                        _loginResult.value =
+                            Result.failure(task.exception ?: Exception("Firebase Auth Failed"))
+                    }
+                }
         } ?: run {
             _loginResult.value = Result.failure(Exception("Google Sign-In Failed"))
         }
