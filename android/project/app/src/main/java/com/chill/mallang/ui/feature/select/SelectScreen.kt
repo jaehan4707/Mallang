202e7a6742af41d3172920e5a8e59c26d1d9baf5
이재한 <99114456+jaehan4707@users.noreply.github.com>
2024-07-19 17:12:50 +0900

    feat(#S11P12D203-23): 회원가입 성공할 경우(가정) 홈으로 이동 추가

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/select/SelectScreen.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/select/SelectScreen.kt
@@ -18,6 +18,8 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -30,6 +32,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.chill.mallang.R
 import com.chill.mallang.ui.component.LongBlackButton
 import com.chill.mallang.ui.component.PercentageBar
@@ -46,7 +49,11 @@ fun SelectScreen(navigateToMain: () -> Unit = {}, signUp: (String) -> Unit = {})
     val viewModel: SelectViewModel = hiltViewModel()
     var selectedTeam by remember { mutableStateOf<String?>(null) }
     var showConfirmButton by remember { mutableStateOf(false) }
+    val isSignUp = viewModel.isSignUp.collectAsStateWithLifecycle()
 
+    if (isSignUp.value) { //회원가입 성공
+        navigateToMain()
+    }
     Surface(
         color = BackGround
     ) {
--- android/project/app/src/main/java/com/chill/mallang/ui/feature/select/SelectViewModel.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/select/SelectViewModel.kt
@@ -4,6 +4,9 @@ import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,19 +14,18 @@ class SelectViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
-    //private val _isSignUp : Mu
-
-    init {
-    }
+    private val _isSignUp = MutableStateFlow<Boolean>(false)
+    val isSignUp = _isSignUp.asStateFlow()
 
     fun login(team: String?) {
         val userName = savedStateHandle.get<String>("userName")
         val userEmail = savedStateHandle.get<String>("userEmail")
         val userProfileImageUrl = savedStateHandle.get<String>("userProfileImageUrl")
         val userNickName = savedStateHandle.get<String>("userNickName")
-        team?.let {
-            //repository -> login 요청
-            // 결과 반환
+        team?.let { //회원가입 로직 후 성공했다면,
+            _isSignUp.update {
+                true
+            }
         }
     }
 }
