eeab4acf2785f5dedd3724dfc0a7ab83a756f34b
이재한 <99114456+jaehan4707@users.noreply.github.com>
2024-07-19 16:34:17 +0900

    feat(#S11P12D203-23): Destination@ 구현

--- android/project/app/src/main/java/com/chill/mallang/ui/navigation/ScreenDestinations.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/navigation/ScreenDestinations.kt
@@ -1,8 +1,10 @@
 package com.chill.mallang.ui.navigation
 
+import android.util.Log
 import androidx.navigation.NamedNavArgument
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
+import com.chill.mallang.ui.feature.login.LoginUiState
 
 /**
  * 기본 라우팅 인터페이스
@@ -12,13 +14,14 @@ interface ScreenDestinations {
     /**
      * NavGraph의 라우팅 경로
      */
-    val route : String
+    val route: String
+
 }
 
 /**
  * Argument를 받을 수 있는 라우팅 인터페이스
  */
-interface ScreenDestinationsWithArgument : ScreenDestinations{
+interface ScreenDestinationsWithArgument : ScreenDestinations {
     /**
      * NavBackStackEntry.arguments.getLong([arg])
      */
@@ -27,17 +30,79 @@ interface ScreenDestinationsWithArgument : ScreenDestinations{
     val arguments: List<NamedNavArgument>
 }
 
-object DestinationMain : ScreenDestinations{
+object DestinationLogin : ScreenDestinations {
+    override val route: String
+        get() = "login"
+
+    fun createRoute(loginUiState: LoginUiState): String {
+        return ""
+    }
+}
+
+object DestinationMain : ScreenDestinations {
     override val route: String
         get() = ""
 }
-object DestinationMap : ScreenDestinations{
+
+object DestinationMap : ScreenDestinations {
     override val route: String
         get() = "map"
 
 }
 
-object DestinationUserDetail : ScreenDestinationsWithArgument{
+object DestinationNickName : ScreenDestinationsWithArgument {
+    override val arg: String
+        get() = "user"
+
+    override val routeWithArgs: String
+        get() = "${this.route}/{userName}/{userEmail}/{userProfileImageUrl}"
+
+    override val arguments: List<NamedNavArgument>
+        get() = listOf(
+            navArgument(name = "userName") { type = NavType.StringType },
+            navArgument(name = "userEmail") { type = NavType.StringType },
+            navArgument(name = "userProfileImageUrl") { type = NavType.StringType }
+        )
+
+    override val route: String
+        get() = "nickname"
+
+    fun createRoute(loginUiState: LoginUiState): String {
+        Log.d("jaehan", "createRoute :${loginUiState}")
+        val route =
+            "${this.route}/${loginUiState.userName}/${loginUiState.userEmail}/${loginUiState.userProfileImageUrl}"
+        Log.d("jaehan","route : ${route}")
+        return route
+    }
+}
+
+object DestinationSelect : ScreenDestinationsWithArgument {
+    override val arg: String
+        get() = "select"
+
+    override val routeWithArgs: String
+        get() = "${this.route}/{userName}/{userEmail}/{userProfileImageUrl}/{userNickName}"
+
+    override val arguments: List<NamedNavArgument>
+        get() = listOf(
+            navArgument(name = "userName") { type = NavType.StringType },
+            navArgument(name = "userEmail") { type = NavType.StringType },
+            navArgument(name = "userProfileImageUrl") { type = NavType.StringType },
+            navArgument(name = "userNickName") { type = NavType.StringType },
+        )
+
+    override val route: String
+        get() = "select"
+
+    fun createRoute(
+        userName: String,
+        userEmail: String,
+        userProfileImageUrl: String,
+        userNickName: String,
+    ) = "${this.route}/${userName}/${userEmail}/${userProfileImageUrl}/${userNickName}"
+}
+
+object DestinationUserDetail : ScreenDestinationsWithArgument {
     override val route: String
         get() = "user_detail"
 
