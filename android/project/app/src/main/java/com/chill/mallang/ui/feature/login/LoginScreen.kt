3c447972e18d1c2b7b299975f5a9c1308c784936
이재한 <99114456+jaehan4707@users.noreply.github.com>
2024-07-23 17:47:07 +0900

    feat(#S11P12D203-84): LoginScreen HandleLoginEvent 추가

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/login/LoginScreen.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/login/LoginScreen.kt
@@ -17,34 +17,44 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.chill.mallang.R
 import com.chill.mallang.ui.theme.BackGround
 import com.chill.mallang.ui.theme.Gray4
 import com.chill.mallang.ui.theme.Gray6
 import com.chill.mallang.ui.theme.Typography
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 
 @Composable
 fun LoginScreen(
-    onLoginSuccess: (LoginUiState) -> Unit
+    onLoginSuccess: (String, String, String) -> Unit,
+    onAuthLoginSuccess: () -> Unit,
 ) {
     val context = LocalContext.current
     val viewModel: LoginViewModel = hiltViewModel()
-
+    val uiState by viewModel.loginUiState.collectAsStateWithLifecycle()
+    val snackBarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
     LaunchedEffect(Unit) {
         viewModel.initCredentialManager(context)
         viewModel.initCredentialRequest()
@@ -61,7 +71,7 @@ fun LoginScreen(
         }
     }
 
-    // Google Sign-In 방식
+// Google Sign-In 방식
     val googleSignInLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.StartActivityForResult()
     ) { result ->
@@ -72,21 +82,22 @@ fun LoginScreen(
         viewModel.setGoogleSignInLauncher(googleSignInLauncher)
     }
 
-    val loginResult by viewModel.loginResult.collectAsState()
-    LaunchedEffect(loginResult) {
-        loginResult?.let { result ->
-            onLoginSuccess(
-                LoginUiState(
-                    userName = result.displayName,
-                    userEmail = result.email,
-                    userProfileImageUrl = URLEncoder.encode(
-                        result.photoUrl.toString(),
-                        StandardCharsets.UTF_8.toString()
-                    )
+    HandleLoginEvent(
+        loginUiState = uiState,
+        authLogin = { onAuthLoginSuccess() },
+        loginSuccess = { userName, userEmail, userProfileImageUrl ->
+            onLoginSuccess(userName, userEmail, userProfileImageUrl)
+        },
+        showSnackBar = { errorMessage ->
+            coroutineScope.launch {
+                snackBarHostState.showSnackbar(
+                    message = errorMessage,
+                    duration = SnackbarDuration.Short,
                 )
-            )
-        }
-    }
+            }
+        },
+    )
+
 
     Surface(
         color = BackGround,
@@ -107,7 +118,7 @@ fun LoginScreen(
             Spacer(modifier = Modifier.weight(0.4f))
             GoogleLoginButton(onClick = {
                 viewModel.initializeLogin(context, credentialLauncher)
-            }, text = "구글 계정으로 로그인하기")
+            }, text = stringResource(R.string.login_button_message))
             Spacer(modifier = Modifier.weight(0.7f))
         }
     }
@@ -145,6 +156,42 @@ fun GoogleLoginButton(
             Text(
                 text, style = Typography.displayLarge
             )
+        }
+    }
+}
+
+@Composable
+fun HandleLoginEvent(
+    loginUiState: LoginUiState,
+    authLogin: () -> Unit,
+    loginSuccess: (String, String, String) -> Unit,
+    showSnackBar: (String) -> Unit,
+) {
+    LaunchedEffect(loginUiState) {
+        when (loginUiState) {
+            LoginUiState.AuthLogin -> {
+                delay(1L)
+                authLogin()
+            }
+
+            is LoginUiState.Success -> {
+                loginSuccess(
+                    loginUiState.userName ?: "",
+                    loginUiState.userEmail ?: "",
+                    URLEncoder.encode(
+                        loginUiState.userProfileImageUrl.toString(),
+                        StandardCharsets.UTF_8.toString()
+                    )
+                )
+            }
+
+            LoginUiState.Loading -> {
+
+            }
+
+            is LoginUiState.Error -> {
+                showSnackBar(loginUiState.errorMessage)
+            }
         }
     }
 }
