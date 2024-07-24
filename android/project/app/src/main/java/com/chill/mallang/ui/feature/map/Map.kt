3259da17f1ec18bc4afc44d72f2864b272a0a7cb
김성수 <skele1207@gmail.com>
2024-07-24 17:17:35 +0900

    feat(#S11P12D203-107): 커스텀 맵 적용

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/map/Map.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/map/Map.kt
@@ -5,14 +5,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.chill.mallang.R
 import com.chill.mallang.ui.feature.map.layout.MapScaffold
-import com.chill.mallang.ui.feature.map.mapview.MapMarker
+import com.chill.mallang.ui.feature.map.mapview.MapView
 import com.chill.mallang.ui.theme.MallangTheme
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 
 
@@ -26,17 +23,7 @@ fun Map(
     }
 
     Column(modifier = modifier.fillMaxSize()){
-        GoogleMap(
-            modifier = Modifier.weight(1f),
-            cameraPositionState = cameraPositionState
-        ) {
-            MapMarker(
-                state = MarkerState(position = singapore),
-                title = "Singapore",
-                snippet = "Marker in Singapore",
-                iconResourceId = R.drawable.ic_location
-            )
-        }
+        MapView()
         MapScaffold()
     }
 }
