f1d5fcc0d106c8ba939131d062b2baebaadb3f76
변서원 <qustjdnjs@naver.com>
2024-07-24 10:33:08 +0900

    ããchange entity

--- backend/mallang/src/main/java/com/chill/mallang/domain/area/dto/AreaDTO.java
+++ backend/mallang/src/main/java/com/chill/mallang/domain/area/dto/AreaDTO.java
@@ -1,15 +1,19 @@
 package com.chill.mallang.domain.area.dto;
 
+import com.chill.mallang.domain.area.model.AreaLog;
 import com.chill.mallang.domain.user.model.User;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
-public class AreaDTO {
+public class AreaLogs {
     private Long id;
     private String name;
     private double latitude;
     private double longitude;
     private User user;
+    private List<AreaLog> areaLogs;
 }
