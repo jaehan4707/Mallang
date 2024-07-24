61b431a70f906d20fa58d755c312fe99aa4c469d
김성수 <skele1207@gmail.com>
2024-07-24 17:16:28 +0900

    feat(#S11P12D203-107): 맵 형태 변경

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/map/Map.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/map/Map.kt
@@ -1,11 +1,13 @@
 package com.chill.mallang.ui.feature.map
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.chill.mallang.R
+import com.chill.mallang.ui.feature.map.layout.MapScaffold
+import com.chill.mallang.ui.feature.map.mapview.MapMarker
 import com.chill.mallang.ui.theme.MallangTheme
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -23,9 +25,9 @@ fun Map(
         position = CameraPosition.fromLatLngZoom(singapore, 10f)
     }
 
-    Box(modifier = modifier.fillMaxSize()){
+    Column(modifier = modifier.fillMaxSize()){
         GoogleMap(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.weight(1f),
             cameraPositionState = cameraPositionState
         ) {
             MapMarker(
