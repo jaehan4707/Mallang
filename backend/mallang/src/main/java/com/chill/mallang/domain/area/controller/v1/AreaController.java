2d3c19956e4e197821236fc7d409e03d29d8d026
변서원 <qustjdnjs@naver.com>
2024-07-25 12:06:17 +0900

    area/사용자 잔여 도전 횟수 조회

--- backend/mallang/src/main/java/com/chill/mallang/domain/area/controller/v1/AreaController.java
+++ backend/mallang/src/main/java/com/chill/mallang/domain/area/controller/v1/AreaController.java
@@ -3,8 +3,11 @@ package com.chill.mallang.domain.area.controller.v1;
 import com.chill.mallang.domain.area.dto.AreaDTO;
 import com.chill.mallang.domain.area.service.AreaService;
 import com.chill.mallang.domain.user.dto.TryCountDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.parameters.P;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +18,34 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/areas")
 public class AreaController {
+    private static final Logger logger = LoggerFactory.getLogger(AreaController.class);
+
     @Autowired
     private AreaService areaService;
+
+    // 1. 점령자 대표 유저 정보 조회
+
+
+    // 2. 사용자 잔여 도전 횟수 조회
+    @GetMapping("/try_count/{userId}")
+    public ResponseEntity<TryCountDTO> getUserTryCountById(@PathVariable Long userId) {
+        try {
+            TryCountDTO tryCount = areaService.getUserTryCountById(userId);
+            return new ResponseEntity<>(tryCount, HttpStatus.OK);
+        } catch (Exception exception) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // 3. 도전 기록 조회
+
+
+    // 4. 그냥 기본 기능
 
     // 점령지 전체 조회
     @GetMapping
     public List<AreaDTO> getAllAreas() {
+        logger.info("api test!!");
         return areaService.getAllAreas();
     }
 
@@ -30,18 +55,4 @@ public class AreaController {
 
         return areaService.getAreaById(id);
     }
-
-    // 2. 사용자 잔여 도전 횟수 조회
-    @GetMapping("/try_count/{userId}")
-    public TryCountDTO getUserTryCountById(@PathVariable Long id) {
-        return areaService.getUserTryCountById(id);
-    }
-
-
-    // 1. 점령자 대표 유저 정보 조회
-
-
-    // 3. 도전 기록 조회
-
-
 }
