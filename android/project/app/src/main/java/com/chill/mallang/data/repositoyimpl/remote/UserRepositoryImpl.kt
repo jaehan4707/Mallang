93a1198484a6892c5c81778dca9bad3f6117f0a6
이재한 <99114456+jaehan4707@users.noreply.github.com>
2024-07-26 17:26:23 +0900

    feat(#S11P12D203-92): UserRepository, UserRepositoryImpl 구현

---
+++ android/project/app/src/main/java/com/chill/mallang/data/repositoyimpl/remote/UserRepositoryImpl.kt
@@ -0,0 +1,77 @@
+package com.chill.mallang.data.repositoyimpl.remote
+
+import com.chill.mallang.data.api.UserApi
+import com.chill.mallang.data.model.apiHandler
+import com.chill.mallang.data.model.request.JoinRequest
+import com.chill.mallang.data.model.request.LoginRequest
+import com.chill.mallang.data.model.response.ApiResponse
+import com.chill.mallang.data.repository.local.DataStoreRepository
+import com.chill.mallang.data.repository.remote.UserRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class UserRepositoryImpl @Inject constructor(
+    private val userApi: UserApi,
+    private val dataStoreRepository: DataStoreRepository,
+) : UserRepository {
+
+    override suspend fun join(request: JoinRequest): Flow<ApiResponse<Boolean>> = flow {
+        val response = apiHandler {
+            userApi.join(request)
+        }
+        when (response) {
+            is ApiResponse.Success -> {
+                emit(ApiResponse.Success(true))
+            }
+
+            is ApiResponse.Error -> {
+                emit(
+                    ApiResponse.Error(
+                        errorCode = response.errorCode,
+                        errorMessage = response.errorMessage
+                    )
+                )
+            }
+
+            ApiResponse.Init -> {
+                emit(ApiResponse.Init)
+            }
+        }
+    }
+
+    override suspend fun logout() {}
+
+    override suspend fun deleteUser() {}
+    override suspend fun login(idToken: String, email: String): Flow<ApiResponse<String>> = flow {
+        val response = apiHandler {
+            userApi.login(
+                LoginRequest(
+                    idToken = idToken,
+                    email = email,
+                )
+            )
+        }
+        when (response) {
+            is ApiResponse.Success -> {
+                val accessToken = response.data ?: ""
+                dataStoreRepository.saveAccessToken(accessToken)
+                emit(ApiResponse.Success(data = accessToken))
+            }
+
+            is ApiResponse.Error -> {
+                emit(
+                    ApiResponse.Error(
+                        errorCode = response.errorCode,
+                        errorMessage = response.errorMessage
+                    )
+                )
+            }
+
+            ApiResponse.Init -> {
+                emit(ApiResponse.Init)
+            }
+        }
+    }
+
+}
