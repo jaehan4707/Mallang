e92854fa92cc66edc877af162bbba24d7c28229f
이재한 <99114456+jaehan4707@users.noreply.github.com>
2024-07-26 17:31:32 +0900

    feat(#S11P12D203-92): SignUpUiState 구현

---
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/select/SignUpUiState.kt
@@ -0,0 +1,22 @@
+package com.chill.mallang.ui.feature.select
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
+
+@Stable
+sealed interface SignUpUiState {
+    @Immutable
+    data class Loading(
+        val userEmail: String? = "",
+        val userNickName: String? = "",
+        val userProfileImageUrl: String? = ""
+    ) : SignUpUiState
+
+    @Immutable
+    data object Success : SignUpUiState
+
+    @Immutable
+    data class Error(
+        val errorMessage: String = "",
+    ) : SignUpUiState
+}
