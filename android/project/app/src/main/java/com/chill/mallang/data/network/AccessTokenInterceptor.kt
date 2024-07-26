c0d0e824da83835c889ee3ecbdca25ded6176dec
이재한 <99114456+jaehan4707@users.noreply.github.com>
2024-07-26 17:28:58 +0900

    feat(#S11P12D203-92): 헤더에 AccessToken 넣어주는 Interceptor 구현

---
+++ android/project/app/src/main/java/com/chill/mallang/data/network/AccessTokenInterceptor.kt
@@ -0,0 +1,34 @@
+package com.chill.mallang.data.network
+
+import com.chill.mallang.data.repository.local.DataStoreRepository
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class AccessTokenInterceptor @Inject constructor(
+    private val dataStoreRepository: DataStoreRepository,
+) :
+    Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val requestBuilder = originalRequest.newBuilder()
+        var accessToken: String? = runBlocking {
+            var token: String? = null
+            dataStoreRepository.getAccessToken().collectLatest { accessToken ->
+                accessToken?.let { // 토큰이 있는 경우
+                    token = accessToken
+                } ?: run {
+                    token = null
+                }
+            }
+            token
+        }
+        val request =
+            requestBuilder.header("Authorization", accessToken ?: "")
+                .build()
+        return chain.proceed(request)
+    }
+}
