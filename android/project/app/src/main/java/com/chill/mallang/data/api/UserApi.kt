e79ef108ae3d5eeba71c81af518ea89a4209a14f
jaehan4707 <jahan4707@naver.com>
2024-07-28 21:49:34 +0900

    feat(#S11P12D203-97): UserApi api 경로 수정 및 반환타입 수정

--- android/project/app/src/main/java/com/chill/mallang/data/api/UserApi.kt
+++ android/project/app/src/main/java/com/chill/mallang/data/api/UserApi.kt
@@ -2,7 +2,7 @@ package com.chill.mallang.data.api
 
 import com.chill.mallang.data.model.request.JoinRequest
 import com.chill.mallang.data.model.request.LoginRequest
-import com.chill.mallang.data.model.response.LoginResponse
+import com.chill.mallang.data.model.response.JoinResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
@@ -10,10 +10,10 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface UserApi {
-    @POST("join")
+    @POST("user/join")
     suspend fun join(
         @Body joinRequest: JoinRequest
-    ): Response<LoginResponse>
+    ): Response<JoinResponse>
 
     @DELETE
     suspend fun deleteUser(): Response<Void>
