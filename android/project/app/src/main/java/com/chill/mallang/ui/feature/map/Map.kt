15278c659d0fbd8c85419cb50c715b4b2b2e4096
김성수 <skele1207@gmail.com>
2024-07-30 14:06:47 +0900

    feat(#S11P12D203-160): StateHoisting 및 사용자 위치에 따른 State 변경
    1. 위치 권한에 따라 점령지 정보 조회
    2. 사용자 위치에 따라 점령지 거리 변동

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/map/Map.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/map/Map.kt
@@ -1,32 +1,78 @@
 package com.chill.mallang.ui.feature.map
 
+import android.Manifest
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.chill.mallang.data.model.Area
 import com.chill.mallang.ui.feature.map.layout.MapScaffold
 import com.chill.mallang.ui.feature.map.mapview.MapView
 import com.chill.mallang.ui.theme.MallangTheme
-import com.google.android.gms.maps.model.CameraPosition
+import com.chill.mallang.ui.util.MultiplePermissionsHandler
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.rememberCameraPositionState
 
-
+/**
+ * @param onShowAreaDetail 점령지 상세 정보 화면으로 이동
+ */
+@SuppressLint("MissingPermission")
 @Composable
 fun Map(
     modifier: Modifier = Modifier,
+    onShowAreaDetail: (Area) -> Unit = {}
 ){
-    val singapore = LatLng(1.35, 103.87)
-    val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    val context = LocalContext.current
+    val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
+    val (currentLocation, setLocation) = remember { mutableStateOf<LatLng?>(null) }
+
+    val (areas, setArea) = remember { mutableStateOf<AreasState>(AreasState.Empty) }
+
+    var hasPermission by remember {
+        mutableStateOf(false)
+    }
+
+    MultiplePermissionsHandler(
+        permissions = listOf( Manifest.permission.ACCESS_FINE_LOCATION ),
+    ) { permissionResults ->
+        if(permissionResults.all { permissions -> permissions.value }){
+            hasPermission = true
+
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                setLocation(LatLng(location.latitude, location.longitude))
+            }
+        } else {
+            // On permission denied
+        }
+    }
+
+    LaunchedEffect(hasPermission) {
+        if(hasPermission){
+            // Get area info
+            setArea(AreasState.HasValue(listOf()))
+        }
     }
 
     Column(modifier = modifier.fillMaxSize()){
         MapView(
-            modifier = Modifier.weight(1f)
+            modifier = Modifier.weight(1f),
+            currentLocation = currentLocation,
+            areasState = areas
         )
-        MapScaffold()
+        MapScaffold(
+            areaSelected = null,
+            currentLocation = null,
+            onLocate = {},
+            onShowDetail = onShowAreaDetail
+        )
     }
 }
 
--- android/project/app/src/main/java/com/chill/mallang/ui/feature/map/mapview/MapView.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/map/mapview/MapView.kt
@@ -1,8 +1,5 @@
 package com.chill.mallang.ui.feature.map.mapview
 
-import CustomMarkerState
-import android.Manifest
-import android.annotation.SuppressLint
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.fadeOut
@@ -13,77 +10,65 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import com.chill.mallang.ui.util.MultiplePermissionsHandler
-import com.google.android.gms.location.LocationServices
+import com.chill.mallang.ui.feature.map.AreasState
+import com.chill.mallang.ui.feature.map.CustomMarkerState
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.SphericalUtil
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapUiSettings
-import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
-
-@SuppressLint("MissingPermission")
 @Composable
 fun MapView(
     modifier: Modifier = Modifier,
-    onLocationPermissionDenied: () -> Unit = {},
-    customMarkers: List<CustomMarkerState> = listOf()
+    currentLocation: LatLng?,
+    areasState: AreasState
 ){
-    val context = LocalContext.current
-    val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
-    var currentLocation by remember { mutableStateOf(MarkerState(LatLng(0.0, 0.0))) }
-    val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(currentLocation.position, 10f)
-    }
+    val cameraPositionState = rememberCameraPositionState()
     val uiSettings = remember {
         MapUiSettings(myLocationButtonEnabled = true)
     }
     val properties by remember {
         mutableStateOf(MapProperties(isMyLocationEnabled = true))
     }
-
     var isMapLoaded by remember {
         mutableStateOf(false)
     }
-    var hasPermission by remember {
-        mutableStateOf(false)
+    val (markerStates, setMarkers) = remember {
+        mutableStateOf(listOf<CustomMarkerState>())
     }
 
+    LaunchedEffect(currentLocation) {
+        currentLocation?.let {
+            cameraPositionState.animate(
+                CameraUpdateFactory.newCameraPosition(
+                    CameraPosition(currentLocation, 15f, 0f, 0f)
+                ),
+                1000
+            )
 
-    MultiplePermissionsHandler(
-        permissions = listOf( Manifest.permission.ACCESS_FINE_LOCATION )
-    ) { permissionResults ->
-        if(permissionResults.all { permissions -> permissions.value }){
-            hasPermission = true
-            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
-                location?.let {
-                    currentLocation = MarkerState(LatLng(it.latitude, it.longitude))
-                    CoroutineScope(Dispatchers.Main).launch {
-                        cameraPositionState.animate(
-                            CameraUpdateFactory.newCameraPosition(
-                                CameraPosition(LatLng(it.latitude, it.longitude), 15f, 0f, 0f)
-                            ),
-                            1000
-                        )
-                    }
-                }
+            for (marker : CustomMarkerState in markerStates){
+                marker.distance = SphericalUtil.computeDistanceBetween(currentLocation, marker.area.latLng).toInt()
             }
-        } else {
-            onLocationPermissionDenied()
         }
     }
+
+    LaunchedEffect(areasState) {
+        if(areasState is AreasState.HasValue){
+            setMarkers(areasState.list.map { area -> CustomMarkerState(area) })
+        }
+    }
+
 
     Box(
         modifier = modifier.fillMaxSize(),
@@ -94,15 +79,17 @@ fun MapView(
             uiSettings = uiSettings,
             properties = properties
         ) {
-            if(hasPermission && isMapLoaded){
-                for (marker : CustomMarkerState in customMarkers){
-                    CustomMarkerWithArea(
-                        state = marker
-                    )
+            if(isMapLoaded ){
+                for (marker : CustomMarkerState in markerStates){
+                    key(keys = arrayOf(marker.area.areaId, marker.distance)) {
+                        CustomMarkerWithArea(
+                            state = marker
+                        )
+                    }
                 }
             }
         }
-        if(!hasPermission || !isMapLoaded) {
+        if(!isMapLoaded) {
             AnimatedVisibility(
                 modifier = Modifier
                     .matchParentSize(),
