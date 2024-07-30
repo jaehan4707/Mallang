b6c30a7c126ca5cc23b6978afa73b6c55ddbf557
이재한 <99114456+jaehan4707@users.noreply.github.com>
2024-07-30 13:06:50 +0900

    feat(#S11P12D203-157): JoinResponse, LoginResponse 파라미터 변경

--- android/project/app/src/main/java/com/chill/mallang/data/model/response/JoinResponse.kt
+++ android/project/app/src/main/java/com/chill/mallang/data/model/response/JoinResponse.kt
@@ -1,11 +1,19 @@
 package com.chill.mallang.data.model.response
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class JoinResponse(
-    val data: Token? = null,
+    val data: JoinData? = null,
+    val status: Int? = null,
+    val success: String? = null,
 )
 
 @Serializable
-data class Token(val token: String? = "")
+data class JoinData(
+    @SerialName("is_registered")
+    val isRegister: Boolean? = false,
+    @SerialName("token")
+    val token: String? = "",
+)
--- android/project/app/src/main/java/com/chill/mallang/data/model/response/LoginResponse.kt
+++ android/project/app/src/main/java/com/chill/mallang/data/model/response/LoginResponse.kt
@@ -1,5 +1,19 @@
 package com.chill.mallang.data.model.response
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class LoginResponse(
-    val data: Boolean? = false,
+    val data: LoginData? = null,
+    val success: String? = null,
+    val code: Int? = null,
+)
+
+@Serializable
+data class LoginData(
+    @SerialName("is_registered")
+    val isRegister: Boolean? = false,
+    @SerialName("token")
+    val token: String? = "",
 )
