a5e701136bbe81db231ed1e86afb42e26e77a8c3
나혜림 <rimhyena@gmail.com>
2024-07-30 17:55:53 +0900

    feat: 닉네임 변경mapping 변경

--- backend/mallang/src/main/java/com/chill/mallang/domain/user/controller/v1/UserSettingController.java
+++ backend/mallang/src/main/java/com/chill/mallang/domain/user/controller/v1/UserSettingController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +26,7 @@ public class UserSettingController {
         this.userSettingService = userSettingService;
     }
 
-    @RequestMapping("/update/nickname")
+    @PatchMapping("/nickname")
     @Operation(summary="user 닉네임 변경 API", description = "해당 유저 닉네임 업데이트 API,헤더에 토큰 첨부 필수")
     public ResponseEntity<?> getUserByEmail(HttpServletRequest request, @RequestBody Map<String, Object> payload) {
         String nickname = (String) payload.get("nickname");
