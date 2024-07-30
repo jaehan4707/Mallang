97e783a276fe467b4b0100f021d6f983463fc0de
NAKYUNG LEE <lnk12888@naver.com>
2024-07-30 15:28:07 +0900

    feat(#S11P12D203-148): QuizResultState에서 expandedAnswer 변수 제거

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/quiz/QuizResultState.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/quiz/QuizResultState.kt
@@ -4,7 +4,6 @@ data class QuizResultState(
     val quizTitle: String = "",
     val quizScript: String = "",
     val wordList: List<Pair<String, String>> = emptyList(),
-    val expandedAnswer: Int = -1,
     val userAnswer: Int = -1,
     val systemAnswer: Int = -1
 )
