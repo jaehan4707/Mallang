dbcdf4879157f2e568a221852aa7c30d3486702b
NAKYUNG LEE <lnk12888@naver.com>
2024-07-30 14:28:28 +0900

    feat(#S11P12D203-147): QuizState, QuizViewModel 구현

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/quiz/QuizState.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/quiz/QuizState.kt
@@ -3,11 +3,7 @@ package com.chill.mallang.ui.feature.quiz
 data class QuizState(
     val quizTitle: String = "",
     val quizScript: String = "",
-    val quizWord: String = "",
-    val wordList: List<Pair<String, String>> = emptyList(),
+    val wordList: List<String> = emptyList(),
     var selectedAnswer: Int = -1,
-    var expandedAnswer: Int = -1,
-    val isResultScreen: Boolean = false,
-    val systemAnswer: Int = -1,
-    val isAnswer: Boolean = false
+    val isResultScreen: Boolean = false
 )
--- android/project/app/src/main/java/com/chill/mallang/ui/feature/quiz/QuizViewModel.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/quiz/QuizViewModel.kt
@@ -22,31 +22,21 @@ class QuizViewModel : ViewModel() {
             state = state.copy(
                 quizTitle = "빈칸을 채워 주세요",
                 quizScript = "우리나라의 경제는 그동안 세계에 유례가 없을 정도로 ___ 할만한 성장을 이루었다.",
-                quizWord = "괄목",
                 wordList = arrayListOf(
-                    "괄목" to "눈을 비비고 볼 정도로 매우 놀라다.",
-                    "상대" to "서로 마주 대하다.",
-                    "과장" to "사실보다 지나치게 불려서 말하거나 표현하다.",
-                    "시기" to "때나 경우."
+                    "괄목",
+                    "상대",
+                    "과장",
+                    "시기"
                 )
             )
         }
     }
 
     fun selectAnswer(index: Int) {
-        if (!state.isResultScreen) {
-            state = state.copy(selectedAnswer = index)
-        } else {
-            state = state.copy(expandedAnswer = index)
-        }
+        state = state.copy(selectedAnswer = index)
     }
 
     fun submitQuiz() {
-        // 퀴즈 제출 로직
-        state = state.copy(
-            isResultScreen = true,
-            systemAnswer = 1, // 임시로 1
-            isAnswer = 1 == state.selectedAnswer
-        )
+        // 퀴즈 채점 api
     }
 }
