79cabeff33c527d4d0673e18cf3ac35f8ee79a40
이재한 <99114456+jaehan4707@users.noreply.github.com>
2024-07-30 16:03:38 +0900

    feat(#S11P12D203-178): NickNameViewModel checkNickName 구현

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/nickname/NicknameViewModel.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/nickname/NicknameViewModel.kt
@@ -5,15 +5,47 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.chill.mallang.data.model.response.ApiResponse
+import com.chill.mallang.data.repository.remote.UserRepository
 import com.chill.mallang.ui.util.ErrorMessage
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class NicknameViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle,
+    private val userRepository: UserRepository,
 ) : ViewModel() {
     val nicknameState = NicknameState()
+    private val _uiState = MutableStateFlow<NickNameUiState>(NickNameUiState.Init)
+    val uiState = _uiState.asStateFlow()
+
+    fun checkNickName() {
+        viewModelScope.launch {
+            userRepository.checkNickName(nickName = nicknameState.nickname)
+                .collectLatest { response ->
+                    when (response) {
+                        is ApiResponse.Success -> {
+                            _uiState.value =
+                                NickNameUiState.Success(nickName = nicknameState.nickname)
+                        }
+
+                        is ApiResponse.Error -> {
+                            nicknameState.updateErrorMessage(ErrorMessage.DUPLICATED_NICKNAME)
+                            _uiState.value =
+                                NickNameUiState.Error(errorMessage = nicknameState.errorMessage)
+                        }
+
+                        ApiResponse.Init -> {}
+                    }
+                }
+        }
+    }
 }
 
 class NicknameState() {
@@ -32,6 +64,10 @@ class NicknameState() {
     fun clearNickname() {
         nickname = ""
         errorMessage = ""
+    }
+
+    fun updateErrorMessage(errorMessage: String) {
+        this.errorMessage = errorMessage
     }
 
     private fun generateErrorMsg(newNickname: String): String {
