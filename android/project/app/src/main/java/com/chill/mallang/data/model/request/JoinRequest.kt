b5ac5e6adff159a11557fde25b5889fc1feaa45e
이재한 <99114456+jaehan4707@users.noreply.github.com>
2024-07-30 13:02:06 +0900

    feat(#S11P12D203-157): JoinRequest idToken 제거 faction 추가

--- android/project/app/src/main/java/com/chill/mallang/data/model/request/JoinRequest.kt
+++ android/project/app/src/main/java/com/chill/mallang/data/model/request/JoinRequest.kt
@@ -5,14 +5,12 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class JoinRequest(
-    @SerialName("token")
-    val idToken: String = "",
     @SerialName("email")
     val userEmail: String = "",
     @SerialName("nickname")
     val userNickName: String = "",
     @SerialName("picture")
     val userProfileImageUrl: String = "",
-//    @SerializedName("team") 서버쪽 개발이 안끝남
-//    val team: String = "",
+    @SerialName("faction")
+    val team: String = "",
 )
