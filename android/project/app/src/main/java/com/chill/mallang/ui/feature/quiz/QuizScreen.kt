1e10ab35c84c70e34ff41b762bb82ba3e2f98faf
NAKYUNG LEE <lnk12888@naver.com>
2024-07-31 10:57:20 +0900

    feat(#S11P12D203-150): 스낵바 띄우는 부분 수정

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/quiz/QuizScreen.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/quiz/QuizScreen.kt
@@ -47,6 +47,7 @@ import com.chill.mallang.ui.component.CustomSnackBar
 import com.chill.mallang.ui.theme.Gray3
 import com.chill.mallang.ui.theme.Gray6
 import com.chill.mallang.ui.theme.MallangTheme
+import com.chill.mallang.ui.theme.Sub1
 import com.chill.mallang.ui.theme.Typography
 import kotlinx.coroutines.launch
 
@@ -56,7 +57,6 @@ fun QuizScreen(
     popUpBackStack: () -> Unit = {},
     navigateToQuizResult: (Int) -> Unit = {},
 ) {
-
     val quizViewModel: QuizViewModel = hiltViewModel()
     val quizState = quizViewModel.state
 
@@ -73,7 +73,7 @@ fun QuizScreen(
         },
         onDismiss = {
             isBackPressed.value = false
-        }
+        },
     )
     BackHandler(onBack = { isBackPressed.value = true })
 
@@ -82,22 +82,27 @@ fun QuizScreen(
             SnackbarHost(
                 hostState = snackbarHostState,
                 snackbar = { snackBarData ->
-                    CustomSnackBar(snackBarData = snackBarData)
-                }
+                    CustomSnackBar(
+                        snackBarData = snackBarData,
+                        backgroundColor = Sub1,
+                        textColor = Color.White,
+                    )
+                },
             )
-        }
+        },
     ) { innerPadding ->
         Box(
-            modifier = modifier
-                .fillMaxSize()
-                .background(color = Color.White)
-                .padding(innerPadding)
+            modifier =
+                modifier
+                    .fillMaxSize()
+                    .background(color = Color.White)
+                    .padding(innerPadding),
         ) {
             Column(modifier = Modifier.fillMaxSize()) {
                 Box(modifier = Modifier.height(15.dp))
                 QuizBox(
                     quizTitle = quizState.quizTitle,
-                    quizScript = quizState.quizScript
+                    quizScript = quizState.quizScript,
                 )
                 Spacer(modifier = Modifier.weight(1f))
                 AnswerList(
@@ -106,7 +111,7 @@ fun QuizScreen(
                     fraction = 0.13f,
                     onAnswerSelected = { selectedIndex ->
                         quizViewModel.selectAnswer(selectedIndex)
-                    }
+                    },
                 )
             }
             Button(
@@ -116,7 +121,7 @@ fun QuizScreen(
                         scope.launch {
                             snackbarHostState.showSnackbar(
                                 message = "정답을 선택해 주세요!",
-                                duration = SnackbarDuration.Short
+                                duration = SnackbarDuration.Short,
                             )
                         }
                     } else {
@@ -124,19 +129,21 @@ fun QuizScreen(
                         navigateToQuizResult(1)
                     }
                 },
-                modifier = Modifier
-                    .align(Alignment.BottomEnd)
-                    .offset(y = (-30).dp) // 버튼을 20dp 위로 올
-                    .widthIn(min = 180.dp) // 버튼의 최소 너비
-                    .heightIn(min = 80.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Gray6
-                ),
-                shape = RoundedCornerShape(20.dp, 0.dp, 0.dp, 20.dp)
+                modifier =
+                    Modifier
+                        .align(Alignment.BottomEnd)
+                        .offset(y = (-30).dp) // 버튼을 20dp 위로 올
+                        .widthIn(min = 180.dp) // 버튼의 최소 너비
+                        .heightIn(min = 80.dp),
+                colors =
+                    ButtonDefaults.buttonColors(
+                        containerColor = Gray6,
+                    ),
+                shape = RoundedCornerShape(20.dp, 0.dp, 0.dp, 20.dp),
             ) {
                 Text(
                     text = "제출하기      >",
-                    style = Typography.headlineLarge
+                    style = Typography.headlineLarge,
                 )
             }
         }
@@ -146,43 +153,45 @@ fun QuizScreen(
 @Composable
 fun QuizBox(
     quizTitle: String,
-    quizScript: String
+    quizScript: String,
 ) {
     Box(
-        modifier = Modifier
-            .padding(12.dp)
-            .border(width = 2.dp, color = Gray6, shape = RoundedCornerShape(10.dp))
-            .fillMaxWidth()
+        modifier =
+            Modifier
+                .padding(12.dp)
+                .border(width = 2.dp, color = Gray6, shape = RoundedCornerShape(10.dp))
+                .fillMaxWidth(),
     ) {
         Column(
             modifier = Modifier.padding(horizontal = 30.dp, vertical = 20.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
+                verticalAlignment = Alignment.CenterVertically,
             ) {
                 Text(
                     text = "Q.",
-                    style = Typography.headlineLarge
+                    style = Typography.headlineLarge,
                 )
                 Box(modifier = Modifier.width(10.dp))
                 Text(
                     text = quizTitle,
-                    style = Typography.headlineMedium
+                    style = Typography.headlineMedium,
                 )
             }
             Box(modifier = Modifier.height(15.dp))
             Spacer(
-                modifier = Modifier
-                    .height(2.dp)
-                    .fillMaxWidth()
-                    .background(Gray3)
+                modifier =
+                    Modifier
+                        .height(2.dp)
+                        .fillMaxWidth()
+                        .background(Gray3),
             )
             Box(modifier = Modifier.height(15.dp))
             Text(
                 text = quizScript,
-                style = Typography.headlineSmall
+                style = Typography.headlineSmall,
             )
         }
     }
@@ -194,9 +203,8 @@ fun AnswerList(
     viewModel: QuizViewModel? = null,
     expandedItem: Int = -1,
     fraction: Float,
-    onAnswerSelected: (Int) -> Unit = { }
+    onAnswerSelected: (Int) -> Unit = { },
 ) {
-
     var size: Int = -1
 
     if (state is QuizState) {
@@ -207,9 +215,10 @@ fun AnswerList(
 
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(15.dp),
-        modifier = Modifier
-            .padding(12.dp)
-            .fillMaxSize()
+        modifier =
+            Modifier
+                .padding(12.dp)
+                .fillMaxSize(),
     ) {
         items(size) { index ->
             if (state is QuizState && viewModel != null) {
@@ -220,7 +229,7 @@ fun AnswerList(
                     state = state,
                     onItemClick = { selectedIndex ->
                         onAnswerSelected(selectedIndex)
-                    }
+                    },
                 )
             } else if (state is QuizResultState) {
                 AnswerResultListItem(
@@ -230,7 +239,7 @@ fun AnswerList(
                     expandedItem = expandedItem,
                     onItemClick = { selectedIndex ->
                         onAnswerSelected(selectedIndex)
-                    }
+                    },
                 )
             }
         }
@@ -243,55 +252,56 @@ fun AnswerListItem(
     index: Int,
     viewModel: QuizViewModel,
     state: QuizState,
-    onItemClick: (Int) -> Unit
+    onItemClick: (Int) -> Unit,
 ) {
     Column {
         Box(
-            modifier = modifier
-                .shadow(
-                    elevation = 5.dp,
-                    shape = RoundedCornerShape(8.dp)
-                )
+            modifier =
+                modifier
+                    .shadow(
+                        elevation = 5.dp,
+                        shape = RoundedCornerShape(8.dp),
+                    ),
         ) {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
-                modifier = modifier
-                    .fillMaxWidth()
-                    .background(color = Color.White, shape = RoundedCornerShape(8.dp))
-                    .border(2.dp, color = Gray6, shape = RoundedCornerShape(8.dp))
-                    .clickable { onItemClick(index) }
+                modifier =
+                    modifier
+                        .fillMaxWidth()
+                        .background(color = Color.White, shape = RoundedCornerShape(8.dp))
+                        .border(2.dp, color = Gray6, shape = RoundedCornerShape(8.dp))
+                        .clickable { onItemClick(index) },
             ) {
                 Box(
-                    modifier = Modifier
-                        .background(
-                            color = Gray6,
-                            shape = RoundedCornerShape(8.dp, 0.dp, 0.dp, 8.dp)
-                        )
-                        .fillMaxHeight()
-                        .width(50.dp),
+                    modifier =
+                        Modifier
+                            .background(
+                                color = Gray6,
+                                shape = RoundedCornerShape(8.dp, 0.dp, 0.dp, 8.dp),
+                            ).fillMaxHeight()
+                            .width(50.dp),
                     contentAlignment = Alignment.Center,
                 ) {
                     Text(
                         text = "${index + 1}",
                         color = Color.White,
                         textAlign = TextAlign.Center,
-                        style = Typography.headlineLarge
+                        style = Typography.headlineLarge,
                     )
                 }
                 Spacer(modifier = Modifier.width(30.dp))
                 Text(
                     text = state.wordList[index],
                     modifier = Modifier.weight(1f),
-                    style = Typography.headlineLarge
+                    style = Typography.headlineLarge,
                 )
                 if (viewModel.selectedAnswer == index + 1) {
                     Icon(
                         modifier = Modifier.padding(10.dp),
                         painter = painterResource(id = R.drawable.ic_check),
-                        contentDescription = null
+                        contentDescription = null,
                     )
                 }
-
             }
         }
     }
