7bd8a0204617d2179bfdf645c22e0b1ae9f827d1
NAKYUNG LEE <lnk12888@naver.com>
2024-07-31 10:57:49 +0900

    chore(#S11P12D203-150): 정렬

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/quiz/QuizViewModel.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/quiz/QuizViewModel.kt
@@ -22,16 +22,18 @@ class QuizViewModel : ViewModel() {
     private fun loadQuizData() {
         viewModelScope.launch {
             // api 호출 및 통신
-            state = state.copy(
-                quizTitle = "빈칸을 채워 주세요",
-                quizScript = "우리나라의 경제는 그동안 세계에 유례가 없을 정도로 ___ 할만한 성장을 이루었다.",
-                wordList = arrayListOf(
-                    "괄목",
-                    "상대",
-                    "과장",
-                    "시기"
+            state =
+                state.copy(
+                    quizTitle = "빈칸을 채워 주세요",
+                    quizScript = "우리나라의 경제는 그동안 세계에 유례가 없을 정도로 ___ 할만한 성장을 이루었다.",
+                    wordList =
+                        arrayListOf(
+                            "괄목",
+                            "상대",
+                            "과장",
+                            "시기",
+                        ),
                 )
-            )
         }
     }
 
