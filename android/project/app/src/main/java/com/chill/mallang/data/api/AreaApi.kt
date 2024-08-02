d47fd5e9b4cdb6a6159d9414ff73f2a3924266cd
김성수 <skele1207@gmail.com>
2024-08-02 14:05:06 +0900

    feat(#S11P12D203-165): 점령지 상세 API 구현

--- android/project/app/src/main/java/com/chill/mallang/data/api/AreaApi.kt
+++ android/project/app/src/main/java/com/chill/mallang/data/api/AreaApi.kt
@@ -2,7 +2,9 @@ package com.chill.mallang.data.api
 
 import com.chill.mallang.data.model.ResponseBody
 import com.chill.mallang.data.model.entity.Area
+import com.chill.mallang.data.model.entity.AreaDetail
 import com.chill.mallang.data.model.entity.TeamList
+import com.chill.mallang.data.model.entity.TeamRecords
 import com.chill.mallang.data.model.entity.TryCount
 import retrofit2.Response
 import retrofit2.http.GET
@@ -19,4 +21,16 @@ interface AreaApi {
     suspend fun getTryCount(
         @Path("userId") userId: Int,
     ): Response<ResponseBody<TryCount>>
+
+    @GET("areas/{areaId}/{userTeam}")
+    suspend fun getAreaDetail(
+        @Path("areaId") areaId: Int,
+        @Path("userTeam") userTeam: Int,
+    ): Response<ResponseBody<AreaDetail>>
+
+    @GET("areas/records/{areaId}/{userId}")
+    suspend fun getAreaRecords(
+        @Path("areaId") areaId: Int,
+        @Path("userId") userId: Int,
+    ): Response<ResponseBody<TeamRecords>>
 }
--- android/project/app/src/main/java/com/chill/mallang/data/repositoyimpl/remote/AreaRepositoryImpl.kt
+++ android/project/app/src/main/java/com/chill/mallang/data/repositoyimpl/remote/AreaRepositoryImpl.kt
@@ -3,7 +3,9 @@ package com.chill.mallang.data.repositoyimpl.remote
 import com.chill.mallang.data.api.AreaApi
 import com.chill.mallang.data.model.apiHandler
 import com.chill.mallang.data.model.entity.Area
+import com.chill.mallang.data.model.entity.AreaDetail
 import com.chill.mallang.data.model.entity.TeamList
+import com.chill.mallang.data.model.entity.TeamRecords
 import com.chill.mallang.data.model.entity.TryCount
 import com.chill.mallang.data.model.response.ApiResponse
 import com.chill.mallang.data.repository.remote.AreaRepository
@@ -61,24 +63,71 @@ class AreaRepositoryImpl
                 }
             }
 
-    override suspend fun getTryCount(areaId: Int): Flow<ApiResponse<TryCount>> =
-        flow {
-            val response = apiHandler { areaApi.getTryCount(areaId) }
-            when (response) {
-                is ApiResponse.Success -> {
-                    emit(ApiResponse.Success(response.data?.data))
-                }
+        override suspend fun getTryCount(areaId: Int): Flow<ApiResponse<TryCount>> =
+            flow {
+                val response = apiHandler { areaApi.getTryCount(areaId) }
+                when (response) {
+                    is ApiResponse.Success -> {
+                        emit(ApiResponse.Success(response.data?.data))
+                    }
 
-                is ApiResponse.Error -> {
-                    emit(
-                        ApiResponse.Error(
-                            errorCode = response.errorCode,
-                            errorMessage = response.errorMessage,
-                        ),
-                    )
-                }
+                    is ApiResponse.Error -> {
+                        emit(
+                            ApiResponse.Error(
+                                errorCode = response.errorCode,
+                                errorMessage = response.errorMessage,
+                            ),
+                        )
+                    }
 
-                ApiResponse.Init -> {}
+                    ApiResponse.Init -> {}
+                }
             }
-        }
-}
+
+        override suspend fun getAreaDetail(
+            areaId: Int,
+            userTeam: Int,
+        ): Flow<ApiResponse<AreaDetail>> =
+            flow {
+                val response = apiHandler { areaApi.getAreaDetail(areaId, userTeam) }
+                when (response) {
+                    is ApiResponse.Success -> {
+                        emit(ApiResponse.Success(response.data?.data))
+                    }
+
+                    is ApiResponse.Error -> {
+                        emit(
+                            ApiResponse.Error(
+                                errorCode = response.errorCode,
+                                errorMessage = response.errorMessage,
+                            ),
+                        )
+                    }
+
+                    ApiResponse.Init -> {}
+                }
+            }
+
+        override suspend fun getAreaRecords(
+            areaId: Int,
+            userId: Int,
+        ): Flow<ApiResponse<TeamRecords>> =
+            flow {
+                val response = apiHandler { areaApi.getAreaRecords(areaId, userId) }
+
+                when (response) {
+                    is ApiResponse.Success -> {
+                        emit(ApiResponse.Success(response.data?.data))
+                    }
+                    is ApiResponse.Error -> {
+                        emit(
+                            ApiResponse.Error(
+                                errorCode = response.errorCode,
+                                errorMessage = response.errorMessage,
+                            )
+                        )
+                    }
+                    ApiResponse.Init -> {}
+                }
+            }
+    }
