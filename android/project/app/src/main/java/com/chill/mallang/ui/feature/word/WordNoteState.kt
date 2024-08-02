098384bd4665b8e386ec3babf97b4b3501e6f95c
NAKYUNG LEE <lnk12888@naver.com>
2024-08-02 17:35:31 +0900

    feat(#S11P12D203-156): HiltViewModel 사용 및 Word 클래스 사용하는 것으로 수정

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/word/WordNoteState.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/word/WordNoteState.kt
@@ -1,6 +1,5 @@
 package com.chill.mallang.ui.feature.word
 
 data class WordNoteState(
-    val wordList: List<WordCard> = emptyList(),
-    val incorrectList: List<IncorrectWord> = emptyList()
+    val wordList: List<Word> = emptyList(),
 )
--- android/project/app/src/main/java/com/chill/mallang/ui/feature/word/WordNoteViewModel.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/word/WordNoteViewModel.kt
@@ -7,62 +7,64 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class WordNoteViewModel : ViewModel() {
-    var state by mutableStateOf(WordNoteState())
-        private set
+class WordNoteViewModel
+    @Inject
+    constructor() : ViewModel() {
+        var state by mutableStateOf(WordNoteState())
+            private set
 
-    init {
-        loadWords()
-    }
+        init {
+            loadWords()
+        }
 
-    // 처음 단어장 정보 불러오기 api
-    private fun loadWords() {
-        viewModelScope.launch {
-            state =
-                state.copy(
-                    wordList =
-                        arrayListOf(
-                            WordCard(
-                                word = "괄목",
-                                meaning = "눈을 비비고 볼 정도로 매우 놀라다.",
-                                example = "우리나라의 경제는 그동안 세계에 유례가 없을 정도로 괄목할 만한 성장을 이루었다.",
+        // 처음 단어장 정보 불러오기 api
+        fun loadWords() {
+            viewModelScope.launch {
+                state =
+                    state.copy(
+                        wordList =
+                            arrayListOf(
+                                Word.WordCard(
+                                    word = "괄목",
+                                    meaning = "눈을 비비고 볼 정도로 매우 놀라다.",
+                                    example = "우리나라의 경제는 그동안 세계에 유례가 없을 정도로 괄목할 만한 성장을 이루었다.",
+                                ),
+                                Word.WordCard(
+                                    word = "상대",
+                                    meaning = "서로 마주 대하다.",
+                                    example = "우리나라의 경제는 그동안 세계에 유례가 없을 정도로 괄목할 만한 성장을 이루었다.",
+                                ),
+                                Word.WordCard(
+                                    word = "과장",
+                                    meaning = "사실보다 지나치게 불려서 말하거나 행동하다.",
+                                    example = "우리나라의 경제는 그동안 세계에 유례가 없을 정도로 괄목할 만한 성장을 이루었다.",
+                                ),
+                                Word.WordCard(
+                                    word = "시기",
+                                    meaning = "때나 경우.",
+                                    example = "우리나라의 경제는 그동안 세계에 유례가 없을 정도로 괄목할 만한 성장을 이루었다.",
+                                ),
                             ),
-                            WordCard(
-                                word = "상대",
-                                meaning = "서로 마주 대하다.",
-                                example = "우리나라의 경제는 그동안 세계에 유례가 없을 정도로 괄목할 만한 성장을 이루었다.",
+                    )
+            }
+        }
+
+        fun loadIncorrectWords() {
+            viewModelScope.launch {
+                state =
+                    state.copy(
+                        wordList =
+                            arrayListOf(
+                                Word.IncorrectWord(
+                                    quizId = 1,
+                                    word = "괄목",
+                                    finish = false,
+                                ),
                             ),
-                            WordCard(
-                                word = "과장",
-                                meaning = "사실보다 지나치게 불려서 말하거나 행동하다.",
-                                example = "우리나라의 경제는 그동안 세계에 유례가 없을 정도로 괄목할 만한 성장을 이루었다.",
-                            ),
-                            WordCard(
-                                word = "시기",
-                                meaning = "때나 경우.",
-                                example = "우리나라의 경제는 그동안 세계에 유례가 없을 정도로 괄목할 만한 성장을 이루었다.",
-                            ),
-                        ),
-                    incorrectList =
-                        arrayListOf(
-                            IncorrectWord(
-                                word = "괄목",
-                                quizTitle = "빈칸을 채워 주세요",
-                                quizScript = "우리나라의 경제는 그동안 세계에 유례가 없을 정도로 괄목할만한 성장을 이루었다.",
-                                wordList =
-                                    arrayListOf(
-                                        "괄목" to "눈을 비비고 볼 정도로 매우 놀라다.",
-                                        "상대" to "서로 마주 대하다.",
-                                        "과장" to "사실보다 지나치게 불려서 말하거나 표현하다.",
-                                        "시기" to "때나 경우.",
-                                    ),
-                                userAnswer = 2,
-                                systemAnswer = 1, // 임시 정답
-                            ),
-                        ),
-                )
+                    )
+            }
         }
     }
-}
