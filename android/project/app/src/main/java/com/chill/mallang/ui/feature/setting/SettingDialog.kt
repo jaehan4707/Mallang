56044547ecd99d6f7e0c1f48c870e9fe3aa732d9
이재한 <99114456+jaehan4707@users.noreply.github.com>
2024-08-02 10:31:40 +0900

    ui(#S11P12D203-176): 배경음,효과음, 푸시알림 스위치 구현

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/setting/SettingDialog.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/setting/SettingDialog.kt
@@ -8,46 +8,50 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Switch
-import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.chill.mallang.R
+import com.chill.mallang.ui.component.MallangSwitch
 import com.chill.mallang.ui.feature.home.ImageButton
 import com.chill.mallang.ui.theme.Gray6
 import com.chill.mallang.ui.theme.MallangTheme
 
 @Composable
-fun SettingDialog(modifier: Modifier = Modifier, onClose: () -> Unit = {}) {
-    var backGroundMusicChecked by remember {
-        mutableStateOf(false)
-    }
-    var effectMusicChecked by remember {
-        mutableStateOf(false)
-    }
+fun SettingDialog(
+    modifier: Modifier = Modifier,
+    onClose: () -> Unit = {},
+) {
+    val (backGroundMusicChecked, setBackGroundMusic) =
+        remember {
+            mutableStateOf(false)
+        }
+    val (effectMusicChecked, setEffectMusic) =
+        remember {
+            mutableStateOf(false)
+        }
+    val (notificationChecked, setNotification) =
+        remember {
+            mutableStateOf(false)
+        }
     Dialog(onDismissRequest = { }) {
         Surface(
             modifier =
-            Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
-                .padding(10.dp),
+                Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .padding(10.dp),
             shape = RoundedCornerShape(15.dp),
             border = BorderStroke(width = 2.dp, color = Gray6),
             color = Color.White,
@@ -55,15 +59,17 @@ fun SettingDialog(modifier: Modifier = Modifier, onClose: () -> Unit = {}) {
             Column(modifier = Modifier.padding(10.dp)) {
                 Row(
                     horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically
+                    verticalAlignment = Alignment.CenterVertically,
                 ) {
                     Spacer(modifier = Modifier.weight(1f))
                     Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.Center) {
-                        Text(text = "설정")
+                        Text(text = stringResource(R.string.setting_title))
                     }
                     Box(
-                        modifier = Modifier
-                            .weight(1f), contentAlignment = Alignment.TopEnd
+                        modifier =
+                            Modifier
+                                .weight(1f),
+                        contentAlignment = Alignment.TopEnd,
                     ) {
                         ImageButton(
                             icon = R.drawable.ic_close,
@@ -73,62 +79,62 @@ fun SettingDialog(modifier: Modifier = Modifier, onClose: () -> Unit = {}) {
                     }
                 }
                 Row(
-                    modifier = Modifier.wrapContentHeight(),
-                    verticalAlignment = Alignment.CenterVertically
+                    modifier =
+                        Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight(),
+                    verticalAlignment = Alignment.CenterVertically,
                 ) {
-                    Text(text = "배경음")
-                    Spacer(modifier = Modifier.weight(0.3f))
-                    Switch(
-                        checked = backGroundMusicChecked,
-                        onCheckedChange = { backGroundMusicChecked = it },
-                        thumbContent = {
-                            if (backGroundMusicChecked) {
-                                Icon(
-                                    painter = painterResource(id = R.drawable.ic_music_on),
-                                    contentDescription = null,
-                                    modifier = Modifier.size(SwitchDefaults.IconSize),
-                                )
-                            } else {
-                                Icon(
-                                    painter = painterResource(id = R.drawable.ic_music_off),
-                                    contentDescription = null,
-                                    modifier = Modifier.size(SwitchDefaults.IconSize),
-                                )
-                            }
-                        }
+                    Text(
+                        text = stringResource(R.string.setting_background_music),
+                        modifier = Modifier.fillMaxWidth(0.25f),
                     )
-                    Spacer(modifier = Modifier.weight(1f))
-                    Text(text = "효과음")
-                    Spacer(modifier = Modifier.weight(0.3f))
-                    Switch(
+                    MallangSwitch(
+                        checked = backGroundMusicChecked,
+                        setChecked = setBackGroundMusic,
+                        onIconImage = R.drawable.ic_volume_on,
+                        offIconImage = R.drawable.ic_volume_off,
+                    )
+                    Spacer(modifier = Modifier.weight(0.1f))
+                    Text(
+                        text = stringResource(R.string.setting_effect_music),
+                        modifier = Modifier.fillMaxWidth(0.4f),
+                    )
+                    MallangSwitch(
                         checked = effectMusicChecked,
-                        onCheckedChange = { effectMusicChecked = it },
-                        thumbContent = {
-                            if (effectMusicChecked) {
-                                Icon(
-                                    painter = painterResource(id = R.drawable.ic_volume_on),
-                                    contentDescription = null,
-                                    modifier = Modifier.size(SwitchDefaults.IconSize),
-                                )
-                            } else {
-                                Icon(
-                                    painter = painterResource(id = R.drawable.ic_volume_off),
-                                    contentDescription = null,
-                                    modifier = Modifier.size(SwitchDefaults.IconSize),
-                                )
-                            }
-                        })
+                        setChecked = setEffectMusic,
+                        onIconImage = R.drawable.ic_volume_on,
+                        offIconImage = R.drawable.ic_volume_off,
+                    )
                 }
-
-
+                Row(
+                    modifier =
+                        Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    Spacer(modifier = Modifier.weight(0.45f))
+                    Text(text = stringResource(R.string.setting_notification))
+                    Spacer(modifier = Modifier.weight(0.1f))
+                    MallangSwitch(
+                        checked = notificationChecked,
+                        setChecked = setNotification,
+                        onIconImage = R.drawable.ic_notification_on,
+                        offIconImage = R.drawable.ic_notification_off,
+                    )
+                    Spacer(modifier = Modifier.weight(0.45f))
+                }
             }
         }
     }
 }
 
+
 @Preview(showBackground = true)
 @Composable
-fun WordNotePreview() {
+fun SettingDialogPreview() {
     MallangTheme {
         SettingDialog()
     }
