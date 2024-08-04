91e826648c4dc59938a4e0606f1b9f26281b4cce
jaehan4707 <jahan4707@naver.com>
2024-08-04 15:21:15 +0900

    feat(#S11P12D203-180): HomeViewModel sharedFlow event 추가, sendEvent를 통한 emit으로 변경

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/home/HomeViewModel.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/home/HomeViewModel.kt
@@ -5,7 +5,9 @@ import androidx.lifecycle.viewModelScope
 import com.chill.mallang.data.model.response.ApiResponse
 import com.chill.mallang.data.repository.remote.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -13,70 +15,48 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel
-    @Inject
-    constructor(
-        private val userRepository: UserRepository,
-    ) : ViewModel() {
-        private val _uiState = MutableStateFlow<HomeUiState>(HomeUiState.Loading)
-        val uiState = _uiState.asStateFlow()
+@Inject
+constructor(
+    private val userRepository: UserRepository,
+) : ViewModel() {
+    private val _uiState = MutableStateFlow<HomeUiState>(HomeUiState.Loading)
+    val uiState = _uiState.asStateFlow()
+    private val _event = MutableSharedFlow<HomeUiEvent>()
+    val event = _event.asSharedFlow()
 
-        init {
-            getUserInfo()
+    init {
+        getUserInfo()
+    }
+
+    fun sendEvent(event: HomeUiEvent) {
+        viewModelScope.launch {
+            _event.emit(event)
         }
+    }
 
-        fun sendEvent(event: HomeUiEvent) {
-            when (event) {
-                HomeUiEvent.ShowSettingDialog -> {
-                    updateHomeUiState { it.copy(showSettingDialog = true) }
-                }
 
-                HomeUiEvent.CloseSettingDialog -> {
-                    updateHomeUiState { it.copy(showSettingDialog = false) }
-                }
-
-                HomeUiEvent.ShowEditNickNameDialog -> {
-                    updateHomeUiState { it.copy(showEditNickNameDialog = true) }
-                }
-
-                HomeUiEvent.CloseEditNickNameDialog -> {
-                    updateHomeUiState {
-                        it.copy(showEditNickNameDialog = false)
-                    }
-                }
-            }
-        }
-
-        private fun updateHomeUiState(update: (HomeUiState.LoadUserInfo) -> HomeUiState.LoadUserInfo) {
-            viewModelScope.launch {
-                val currentState = _uiState.value
-                if (currentState is HomeUiState.LoadUserInfo) {
-                    _uiState.value = update(currentState)
-                }
-            }
-        }
-
-        private fun getUserInfo() {
-            viewModelScope.launch {
-                userRepository.getUserInfo().collectLatest { response ->
-                    _uiState.value =
-                        when (response) {
-                            is ApiResponse.Error -> {
-                                HomeUiState.Error(
-                                    errorCode = response.errorCode,
-                                    errorMessage = response.errorMessage,
-                                )
-                            }
-
-                            is ApiResponse.Success -> {
-                                HomeUiState.LoadUserInfo(
-                                    userNickName = response.data?.nickName ?: "",
-                                    userFaction = response.data?.faction ?: "",
-                                )
-                            }
-
-                            ApiResponse.Init -> HomeUiState.Loading
+    private fun getUserInfo() {
+        viewModelScope.launch {
+            userRepository.getUserInfo().collectLatest { response ->
+                _uiState.value =
+                    when (response) {
+                        is ApiResponse.Error -> {
+                            HomeUiState.Error(
+                                errorCode = response.errorCode,
+                                errorMessage = response.errorMessage,
+                            )
                         }
-                }
+
+                        is ApiResponse.Success -> {
+                            HomeUiState.LoadUserInfo(
+                                userNickName = response.data?.nickName ?: "",
+                                userFaction = response.data?.faction ?: "",
+                            )
+                        }
+
+                        ApiResponse.Init -> HomeUiState.Loading
+                    }
             }
         }
     }
+}
