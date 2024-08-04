b1ec3583cbd1779854c0acf01676c6760116c3cd
SeungSoo <skele1207@gmail.com>
2024-08-04 22:36:43 +0900

    feat(#S11P12D203): ViewModel이 Application 전역에서 같은 데이터를 사용하도록 변경.
    Singleton Repository를 사용해서 Viewmodel에 주입

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/topbar/MallangTopbar.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/topbar/MallangTopbar.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,6 +23,7 @@ fun MallangTopbar(
     navController: NavController,
 ) {
     val viewModel: TopbarViewModel = hiltViewModel()
+    val title by viewModel.titleContent
 
     Row(
         modifier = modifier.fillMaxWidth(),
@@ -31,7 +33,7 @@ fun MallangTopbar(
         BackButtion(
             onBack = { viewModel.onBack(navController) },
         )
-        viewModel.titleContent
+        title()
         HomeButton(
             onHome = { viewModel.onHome(navController) },
         )
--- android/project/app/src/main/java/com/chill/mallang/ui/feature/topbar/TopbarViewModel.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/topbar/TopbarViewModel.kt
@@ -3,8 +3,6 @@ package com.chill.mallang.ui.feature.topbar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
@@ -17,29 +15,25 @@ class TopbarViewModel
     @Inject
     constructor(
         private val savedStateHandle: SavedStateHandle,
+        private val topbarRepository: TopbarRepository,
     ) : ViewModel() {
-        var titleContent by mutableStateOf<@Composable () -> Unit>({})
-            private set
-        var onBack by mutableStateOf<(NavController) -> Unit>({})
-            private set
-        var onHome by mutableStateOf<(NavController) -> Unit>({})
-            private set
+        val titleContent = topbarRepository.titleContent
+        val onBack by topbarRepository.onBack
+        val onHome by topbarRepository.onHome
 
         fun updateTitle(title: String) {
-            titleContent = {
-                Text(text = title, style = Typography.titleMedium)
-            }
+            topbarRepository.updateTitle { Text(text = title, style = Typography.titleMedium) }
         }
 
         fun updateTitle(title: @Composable () -> Unit) {
-            titleContent = title
+            topbarRepository.updateTitle(title)
         }
 
         fun updateOnBack(onBack: (NavController) -> Unit) {
-            this.onBack = onBack
+            topbarRepository.updateOnBack(onBack)
         }
 
         fun updateOnHome(onHome: (NavController) -> Unit) {
-            this.onHome = onHome
+            topbarRepository.updateOnHome(onHome)
         }
     }
