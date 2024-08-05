48935e8b1f326839a9c1f2632b1988c010652c8e
강승우 <pooya98@naver.com>
2024-08-05 10:25:57 +0900

    ui(#S11P12D203-194): 루트 게임 컨테이너 화면 생성 및 StateHolder 연결

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/game/GameScreen.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/game/GameScreen.kt
@@ -1,22 +1,32 @@
 package com.chill.mallang.ui.feature.game
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.chill.mallang.ui.feature.game.game01.Game01Screen
+import com.chill.mallang.ui.feature.game.game02.Game02Screen
+import com.chill.mallang.ui.feature.game.game03.Game03Screen
 import com.chill.mallang.ui.theme.MallangTheme
 
 @Composable
-fun GameScreen(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(top = 20.dp)
-    ) {
-        
+fun GameScreen(
+    gameType: GameType,
+    modifier: Modifier = Modifier,
+) {
+    val viewModel: GameViewModel = hiltViewModel()
+
+    LaunchedEffect(Unit) {
+        viewModel.initGameType(gameType)
+        viewModel.updateGameState(gameState = GameState.READY)
+    }
+
+    when (viewModel.gameType) {
+        GameType.INIT -> TODO()
+        GameType.GAME01 -> Game01Screen()
+        GameType.GAME02 -> Game02Screen()
+        GameType.GAME03 -> Game03Screen()
     }
 }
 
@@ -24,6 +34,6 @@ fun GameScreen(modifier: Modifier = Modifier) {
 @Composable
 fun GameLobbyScreenPreview() {
     MallangTheme {
-        GameScreen()
+        GameScreen(GameType.GAME01)
     }
 }
