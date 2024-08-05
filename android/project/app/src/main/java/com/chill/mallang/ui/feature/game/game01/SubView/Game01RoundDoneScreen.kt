c381beffe6f27dba9c6d20121385d8dba5ff183a
강승우 <pooya98@naver.com>
2024-08-05 10:19:57 +0900

    ui(#S11P12D203-194): 게임01 라운드 완료 화면 UI 구현

---
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/game/game01/SubView/Game01RoundDoneScreen.kt
@@ -0,0 +1,41 @@
+package com.chill.mallang.ui.feature.game.game01.SubView
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.chill.mallang.ui.theme.MallangTheme
+
+@Composable
+fun Game01RoundDoneScreen(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CircularProgressIndicator()
+            Spacer(modifier = Modifier.height(50.dp))
+            Text(text = "제출 완료!")
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun Game01RoundDoneScreenPreview() {
+    MallangTheme {
+        Game01LoadingScreen()
+    }
+}
