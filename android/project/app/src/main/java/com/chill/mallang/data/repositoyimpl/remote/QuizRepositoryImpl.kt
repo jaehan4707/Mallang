401c2700de91b211a7ec55395275d4fcc01df664
SeungwooKang <pooya98@naver.com>
2024-08-06 17:51:57 +0900

    feat(#S11P12D203-196): Quiz Repository, RepositoryImpl 생성

---
+++ android/project/app/src/main/java/com/chill/mallang/data/repositoyimpl/remote/QuizRepositoryImpl.kt
@@ -0,0 +1,115 @@
+package com.chill.mallang.data.repositoyimpl.remote
+
+import com.chill.mallang.data.api.QuizApi
+import com.chill.mallang.data.model.apiHandler
+import com.chill.mallang.data.model.entity.Game01PlayResult
+import com.chill.mallang.data.model.entity.Game01QuizData
+import com.chill.mallang.data.model.request.FetchGameResultRequest
+import com.chill.mallang.data.model.request.GradingUserAnswerRequest
+import com.chill.mallang.data.model.response.ApiResponse
+import com.chill.mallang.data.repository.remote.QuizRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class QuizRepositoryImpl
+    @Inject
+    constructor(
+        private val quizApi: QuizApi,
+    ) : QuizRepository {
+        override suspend fun getQuizIds(areaId: Int): Flow<ApiResponse<List<Int>>> =
+            flow {
+                val response =
+                    apiHandler {
+                        quizApi.getQuizIds(areaId)
+                    }
+                when (response) {
+                    is ApiResponse.Success -> {
+                        emit(ApiResponse.Success(response.data?.data))
+                    }
+
+                    is ApiResponse.Error -> {
+                        emit(
+                            ApiResponse.Error(
+                                errorCode = response.errorCode,
+                                errorMessage = response.errorMessage,
+                            ),
+                        )
+                    }
+
+                    is ApiResponse.Init -> {}
+                }
+            }
+
+        override suspend fun getQuiz(quizId: Int): Flow<ApiResponse<Game01QuizData>> =
+            flow {
+                val response =
+                    apiHandler {
+                        quizApi.getQuiz(quizId)
+                    }
+                when (response) {
+                    is ApiResponse.Success -> {
+                        emit(ApiResponse.Success(response.data?.data))
+                    }
+
+                    is ApiResponse.Error -> {
+                        emit(
+                            ApiResponse.Error(
+                                errorCode = response.errorCode,
+                                errorMessage = response.errorMessage,
+                            ),
+                        )
+                    }
+
+                    is ApiResponse.Init -> {}
+                }
+            }
+
+        override suspend fun postUserAnswer(gradingUserAnswerRequest: GradingUserAnswerRequest): Flow<ApiResponse<Unit>> =
+            flow {
+                val response =
+                    apiHandler {
+                        quizApi.submitUserAnswer(gradingUserAnswerRequest)
+                    }
+                when (response) {
+                    is ApiResponse.Success -> {
+                        emit(ApiResponse.Success(response.data))
+                    }
+
+                    is ApiResponse.Error -> {
+                        emit(
+                            ApiResponse.Error(
+                                errorCode = response.errorCode,
+                                errorMessage = response.errorMessage,
+                            ),
+                        )
+                    }
+
+                    is ApiResponse.Init -> {}
+                }
+            }
+
+        override suspend fun getResults(fetchGameResultRequest: FetchGameResultRequest): Flow<ApiResponse<Game01PlayResult>> =
+            flow {
+                val response =
+                    apiHandler {
+                        quizApi.postFinalDataAndFetchResult(fetchGameResultRequest)
+                    }
+                when (response) {
+                    is ApiResponse.Success -> {
+                        emit(ApiResponse.Success(response.data?.data))
+                    }
+
+                    is ApiResponse.Error -> {
+                        emit(
+                            ApiResponse.Error(
+                                errorCode = response.errorCode,
+                                errorMessage = response.errorMessage,
+                            ),
+                        )
+                    }
+
+                    is ApiResponse.Init -> {}
+                }
+            }
+    }
