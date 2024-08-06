a865b25b7a4120a30ead6d1b030c90cc836e2f3c
김성수 <skele1207@gmail.com>
2024-08-06 16:15:24 +0900

    feat(#S11P12D203-276): 배경음, 효과음 기능 구현
    배경음
    - 반복되는 음악 재생
    - 재생/일시정지/정지/음량조절
    효과음
    - 짧은 소리 재생
    - 재생/음량조절

--- android/project/app/src/main/java/com/chill/mallang/ui/sound/SoundManager.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/sound/SoundManager.kt
@@ -14,29 +14,124 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 
+/**
+ * ### 배경음/효과음 매니저
+ *
+ * 앱 전역에서 사용가능한 음악 플레이어.
+ *
+ * `backgroundMusic` 관련 함수로 긴 배경음악을 재생.
+ *
+ * `soundEffect` 관련 함수로 효과음을 재생.
+ *
+ * ViewModel로의 주입법
+ * ```kotlin
+ * @Inject
+ * constructor(
+ *     private val savedStateHandle: SavedStateHandle,
+ *     val soundManager: SoundManager,
+ * ) : ViewModel()
+ * ```
+ * Composable에서의 사용법
+ * ```kotlin
+ * DisposableEffect(Unit) {
+ *     viewModel.soundManager.playBackgroundMusic(R.raw.background)
+ *     onDispose {
+ *         viewModel.soundManager.stopBackgroundMusic()
+ *     }
+ * }
+ * ```
+ */
 @Singleton
-class SoundManager @Inject constructor(
-    @ApplicationContext private val context: Context,
-    @ApplicationScope private val applicationScope: CoroutineScope,
-    private val settingsStoreRepository: SettingsStoreRepository,
-) {
-    private var mediaPlayer: MediaPlayer = MediaPlayer.create(context, 1)
-    private var soundPool: SoundPool = SoundPool.Builder().build()
+class SoundManager
+    @Inject
+    constructor(
+        @ApplicationContext private val context: Context,
+        @ApplicationScope private val applicationScope: CoroutineScope,
+        private val settingsStoreRepository: SettingsStoreRepository,
+    ) {
+        private var currentBackgroundId: Int? = null
 
-    private val _backgroundVolume: MutableStateFlow<Float> = MutableStateFlow(1f)
-    val backgroundVolume: StateFlow<Float> = _backgroundVolume.asStateFlow()
+        private var mediaPlayer: MediaPlayer? = null
+        private var soundPool: SoundPool = SoundPool.Builder().build()
 
-    private val _soundEffectsVolume: MutableStateFlow<Float> = MutableStateFlow(1f)
-    val soundEffectsVolume: StateFlow<Float> = _soundEffectsVolume.asStateFlow()
+        private val _backgroundVolume: MutableStateFlow<Float> = MutableStateFlow(1f)
+        val backgroundVolume: StateFlow<Float> = _backgroundVolume.asStateFlow()
 
-    private val _notificationAlarm: MutableStateFlow<Boolean> = MutableStateFlow(false)
-    val notificationAlarm: StateFlow<Boolean> = _notificationAlarm.asStateFlow()
+        private val _soundEffectsVolume: MutableStateFlow<Float> = MutableStateFlow(1f)
+        val soundEffectsVolume: StateFlow<Float> = _soundEffectsVolume.asStateFlow()
 
-    init {
-        applicationScope.launch {
-            _backgroundVolume.value = settingsStoreRepository.getBackgroundVolume()
-            _soundEffectsVolume.value = settingsStoreRepository.getSoundEffectsVolume()
-            _notificationAlarm.value = settingsStoreRepository.getNotificationAlarm()
+        private val _notificationAlarm: MutableStateFlow<Boolean> = MutableStateFlow(false)
+        val notificationAlarm: StateFlow<Boolean> = _notificationAlarm.asStateFlow()
+
+        init {
+            applicationScope.launch {
+                _backgroundVolume.value = settingsStoreRepository.getBackgroundVolume()
+                _soundEffectsVolume.value = settingsStoreRepository.getSoundEffectsVolume()
+                _notificationAlarm.value = settingsStoreRepository.getNotificationAlarm()
+            }
+        }
+
+        fun playBackgroundMusic(resId: Int) {
+            if (currentBackgroundId == resId && mediaPlayer?.isPlaying == true) return
+
+            if (mediaPlayer == null) {
+                mediaPlayer = MediaPlayer.create(context, resId)
+            } else {
+                mediaPlayer?.run {
+                    if (isPlaying) stop()
+                    reset()
+                    try {
+                        val afd = context.resources.openRawResourceFd(resId)
+                        setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
+                        prepare()
+                    } catch (e: Exception) {
+                        return
+                    }
+                }
+            }
+            mediaPlayer?.run {
+                isLooping = true
+                setVolume(_backgroundVolume.value, _backgroundVolume.value)
+                start()
+                currentBackgroundId = resId
+            }
+        }
+
+        fun stopBackgroundMusic() {
+            mediaPlayer?.run {
+                stop()
+                prepare()
+            }
+        }
+
+        fun pauseBackgroundMusic() {
+            mediaPlayer?.pause()
+        }
+
+        fun resumeBackgroundMusic() {
+            mediaPlayer?.run {
+                if (!isPlaying) mediaPlayer?.start()
+            }
+        }
+
+        fun playSoundEffect(soundResId: Int) {
+            val soundId = soundPool.load(context, soundResId, 1)
+            soundPool.setVolume(soundId, _soundEffectsVolume.value, _soundEffectsVolume.value)
+        }
+
+        fun setBackgroundVolume(volume: Float) {
+            applicationScope.launch {
+                settingsStoreRepository.setBackgroundVolume(volume)
+                _backgroundVolume.value = volume
+                mediaPlayer?.setVolume(volume, volume)
+            }
+        }
+
+        fun setSoundEffectsVolume(volume: Float) {
+            _soundEffectsVolume.value = volume
+        }
+
+        fun setNotificationAlarm(alarm: Boolean) {
+            _notificationAlarm.value = alarm
         }
     }
-}
