73d3733ab7071443127f8a8ad1ee94cf5b2739af
나혜림 <rimhyena@gmail.com>
2024-08-06 13:03:06 +0900

    Feat S11P12D203-243: User 정보조회 -> userid추가+ factionName->factionID로 변경

--- backend/mallang/src/main/java/com/chill/mallang/domain/user/dto/FindByEmailDTO.java
+++ backend/mallang/src/main/java/com/chill/mallang/domain/user/dto/FindByEmailDTO.java
@@ -7,13 +7,17 @@ import lombok.Setter;
 @Getter
 @Setter
 public class FindByEmailDTO {
+    private long id;
+    private String email;
     private String nickname;
-    private String faction;
+    private long faction_id;
     private Integer try_count;
 
-    public FindByEmailDTO(String nickname, String faction, Integer tryCount) {
+    public FindByEmailDTO(long id, String email, String nickname, long faction_id, Integer tryCount) {
+        this.id = id;
+        this.email = email;
         this.nickname = nickname;
-        this.faction = faction;
+        this.faction_id = faction_id;
         this.try_count = tryCount;
     }
 }
