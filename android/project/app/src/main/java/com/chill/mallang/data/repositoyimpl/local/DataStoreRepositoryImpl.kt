a9a667bb4946d0b71860bb199ecb93a2c40d524d
이재한 <99114456+jaehan4707@users.noreply.github.com>
2024-08-06 17:46:51 +0900

    feat(#S11P12D203-287): DataStoreRepository, DataStoreRepositoryImpl saveUser, getUserId, getFactionId 추가

--- android/project/app/src/main/java/com/chill/mallang/data/repositoyimpl/local/DataStoreRepositoryImpl.kt
+++ android/project/app/src/main/java/com/chill/mallang/data/repositoyimpl/local/DataStoreRepositoryImpl.kt
@@ -4,6 +4,7 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
+import com.chill.mallang.data.model.entity.User
 import com.chill.mallang.data.model.response.ApiResponse
 import com.chill.mallang.data.repository.local.DataStoreRepository
 import kotlinx.coroutines.flow.Flow
@@ -72,13 +73,47 @@ class DataStoreRepositoryImpl
                     emailDeletionResult.exceptionOrNull()?.message?.let { errors.add(it) }
                     tokenDeletionResult.exceptionOrNull()?.message?.let { errors.add(it) }
                     if (errors.isNotEmpty()) {
-                        emit(ApiResponse.Error(errorCode = 500, errorMessage = errors.joinToString(", ")))
+                        emit(
+                            ApiResponse.Error(
+                                errorCode = 500,
+                                errorMessage = errors.joinToString(", "),
+                            ),
+                        )
                     }
                 }
+            }
+
+        override suspend fun saveUser(user: User) {
+            dataStore.edit { prefs ->
+                prefs[USER_ID_KEY] = user.id.toString()
+                prefs[USER_FACTION_ID] = user.factionId.toString()
+            }
+        }
+
+        override suspend fun getUserId() =
+            flow {
+                emit(
+                    dataStore.data
+                        .map { prefs ->
+                            prefs[USER_ID_KEY].toString().toIntOrNull()
+                        }.first(),
+                )
+            }
+
+        override suspend fun getFactionId() =
+            flow {
+                emit(
+                    dataStore.data
+                        .map { prefs ->
+                            prefs[USER_FACTION_ID].toString().toIntOrNull()
+                        }.first(),
+                )
             }
 
         companion object {
             val ACCESS_TOKEN_KEY = stringPreferencesKey("ACCESS_TOKEN_KEY")
             val USER_EMAIL_KEY = stringPreferencesKey("USER_EMAIL_KEY")
+            val USER_FACTION_ID = stringPreferencesKey("USER_FACTION_ID_KEY")
+            val USER_ID_KEY = stringPreferencesKey("USER_ID_KEY")
         }
     }
