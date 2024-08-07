eba4e7d0187fdfea1ba2e79a3d5f1724f5666b97
NAKYUNG LEE <lnk12888@naver.com>
2024-08-07 16:04:37 +0900

    chore(#S11P12D203-229): 정렬

--- android/project/app/src/main/java/com/chill/mallang/data/network/AccessTokenInterceptor.kt
+++ android/project/app/src/main/java/com/chill/mallang/data/network/AccessTokenInterceptor.kt
@@ -7,28 +7,31 @@ import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
 
-class AccessTokenInterceptor @Inject constructor(
-    private val dataStoreRepository: DataStoreRepository,
-) :
-    Interceptor {
-
-    override fun intercept(chain: Interceptor.Chain): Response {
-        val originalRequest = chain.request()
-        val requestBuilder = originalRequest.newBuilder()
-        var accessToken: String? = runBlocking {
-            var token: String? = null
-            dataStoreRepository.getAccessToken().collectLatest { accessToken ->
-                accessToken?.let { // 토큰이 있는 경우
-                    token = accessToken
-                } ?: run {
-                    token = null
+class AccessTokenInterceptor
+    @Inject
+    constructor(
+        private val dataStoreRepository: DataStoreRepository,
+    ) : Interceptor {
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val originalRequest = chain.request()
+            val requestBuilder = originalRequest.newBuilder()
+            var accessToken: String? =
+                runBlocking {
+                    var token: String? = null
+                    dataStoreRepository.getAccessToken().collectLatest { accessToken ->
+                        accessToken?.let {
+                            // 토큰이 있는 경우
+                            token = accessToken
+                        } ?: run {
+                            token = null
+                        }
+                    }
+                    token
                 }
-            }
-            token
+            val request =
+                requestBuilder
+                    .header("Authorization", "Bearer $accessToken")
+                    .build()
+            return chain.proceed(request)
         }
-        val request =
-            requestBuilder.header("Authorization", "Bearer $accessToken")
-                .build()
-        return chain.proceed(request)
     }
-}
