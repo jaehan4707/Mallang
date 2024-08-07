0a80bc18a45ea8e151845fd3e7b7348c06184b72
NAKYUNG LEE <lnk12888@naver.com>
2024-08-07 09:23:46 +0900

    feat(#S11P12D203-228): data class api 맞추어 변경

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/study_result/StudyResultState.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/study_result/StudyResultState.kt
@@ -4,6 +4,5 @@ data class StudyResultState(
     val quizTitle: String = "",
     val quizScript: String = "",
     val wordList: List<Pair<String, String>> = emptyList(),
-    val userAnswer: Int = -1,
-    val systemAnswer: Int = -1
+    val result: Boolean,
 )
