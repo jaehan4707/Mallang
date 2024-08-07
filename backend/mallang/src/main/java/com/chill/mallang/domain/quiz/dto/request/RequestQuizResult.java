e7b0091b72129cffb9138ef9eea1cddc0be2b583
Iantoo2 <rlawjddla0203@gmail.com>
2024-08-07 15:29:49 +0900

    Add [S11P12D203-265] : Post 요청 값 확인 코드 추가

--- backend/mallang/src/main/java/com/chill/mallang/domain/quiz/dto/request/RequestQuizResult.java
+++ backend/mallang/src/main/java/com/chill/mallang/domain/quiz/dto/request/RequestQuizResult.java
@@ -1,14 +1,16 @@
 package com.chill.mallang.domain.quiz.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class RequestQuizResult {
+    @NotNull
     private Long areaID;
+    @NotNull
     private Long userID;
+    @NotNull
     private Long factionID;
+    @NotNull
     private Long[] quizID;
-    // 최종제출 여부 0 or 1 -> 최종 적으로 True;
-    // 1 인 경우만 결과 도축
-    // quizID userID -> Answer 제일 최신 순서로 False
 }
