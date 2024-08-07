67348c7e601472dbc9f2d5bc36a09fc652069937
NAKYUNG LEE <lnk12888@naver.com>
2024-08-07 16:05:08 +0900

    feat(#S11P12D203-229): 단어장/오답노트 목록 불러오는 api 추가

---
+++ android/project/app/src/main/java/com/chill/mallang/data/repository/remote/StudyRepository.kt
@@ -0,0 +1,11 @@
+package com.chill.mallang.data.repository.remote
+
+import com.chill.mallang.data.model.response.ApiResponse
+import com.chill.mallang.ui.feature.word.Word
+import kotlinx.coroutines.flow.Flow
+
+interface StudyRepository {
+    suspend fun getWordList(userId: Int): Flow<ApiResponse<List<Word.CorrectWord>>>
+
+    suspend fun getIncorrectList(userId: Int): Flow<ApiResponse<List<Word.IncorrectWord>>>
+}
---
+++ android/project/app/src/main/java/com/chill/mallang/data/repositoyimpl/remote/StudyRepositoryImpl.kt
@@ -0,0 +1,87 @@
+package com.chill.mallang.data.repositoyimpl.remote
+
+import com.chill.mallang.data.api.StudyApi
+import com.chill.mallang.data.model.apiHandler
+import com.chill.mallang.data.model.response.ApiResponse
+import com.chill.mallang.data.repository.remote.StudyRepository
+import com.chill.mallang.ui.feature.word.Word
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class StudyRepositoryImpl
+    @Inject
+    constructor(
+        private val studyApi: StudyApi,
+    ) : StudyRepository {
+        override suspend fun getWordList(userId: Int): Flow<ApiResponse<List<Word.CorrectWord>>> =
+            flow {
+                val response =
+                    apiHandler {
+                        studyApi.getWordList(userId = userId)
+                    }
+                when (response) {
+                    is ApiResponse.Success -> {
+                        emit(
+                            ApiResponse.Success(
+                                response.body?.map {
+                                    Word.CorrectWord(
+                                        word = it.word ?: "",
+                                        example = it.example ?: "",
+                                        meaning = it.meaning ?: "",
+                                    )
+                                },
+                            ),
+                        )
+                    }
+
+                    is ApiResponse.Error -> {
+                        emit(
+                            ApiResponse.Error(
+                                errorCode = response.errorCode,
+                                errorMessage = response.errorMessage,
+                            ),
+                        )
+                    }
+
+                    ApiResponse.Init -> {
+                        emit(ApiResponse.Init)
+                    }
+                }
+            }
+
+        override suspend fun getIncorrectList(userId: Int): Flow<ApiResponse<List<Word.IncorrectWord>>> =
+            flow {
+                val response =
+                    apiHandler {
+                        studyApi.getIncorrectList(userId = userId)
+                    }
+                when (response) {
+                    is ApiResponse.Success -> {
+                        emit(
+                            ApiResponse.Success(
+                                response.body?.map {
+                                    Word.IncorrectWord(
+                                        studyId = it.studyId ?: -1,
+                                        word = it.word ?: "",
+                                    )
+                                },
+                            ),
+                        )
+                    }
+
+                    is ApiResponse.Error -> {
+                        emit(
+                            ApiResponse.Error(
+                                errorCode = response.errorCode,
+                                errorMessage = response.errorMessage,
+                            ),
+                        )
+                    }
+
+                    is ApiResponse.Init -> {
+                        emit(ApiResponse.Init)
+                    }
+                }
+            }
+    }
