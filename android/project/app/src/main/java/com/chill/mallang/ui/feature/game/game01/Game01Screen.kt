b12d3eef26e5db898d121f1c160057b756a5edee
강승우 <pooya98@naver.com>
2024-08-08 17:24:14 +0900

    feat(#S11P12D203-198): 게임01 컨테이너 화면 UiEvent 핸들러 추가

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/game/game01/Game01Screen.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/game/game01/Game01Screen.kt
@@ -1,11 +1,13 @@
 package com.chill.mallang.ui.feature.game.game01
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.chill.mallang.R
+import com.chill.mallang.ui.feature.game.game01.Game01ViewModel.GameConstants.ROUND_COUNT
 import com.chill.mallang.ui.feature.game.game01.SubView.Game01LoadingScreen
 import com.chill.mallang.ui.feature.game.game01.SubView.Game01PlayScreen
 import com.chill.mallang.ui.feature.game.game01.SubView.Game01ResultScreen
@@ -13,10 +15,24 @@ import com.chill.mallang.ui.feature.game.game01.SubView.Game01ReviewScreen
 import com.chill.mallang.ui.feature.game.game01.SubView.Game01RoundDoneScreen
 import com.chill.mallang.ui.feature.game.game01.SubView.Game01RoundScreen
 import com.chill.mallang.ui.theme.MallangTheme
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun Game01Screen(modifier: Modifier = Modifier) {
     val viewModel: Game01ViewModel = hiltViewModel()
+
+    HandleGame01Event(
+        game01UiEvent = viewModel.gameUiEvent,
+        fetchQuizIdsInfo = { viewModel.fetchQuizIds() },
+        fetchQuizData = { viewModel.fetchQuiz() },
+        fetchFinalResult = { viewModel.fetchFinalResult() },
+        updateGame01StateToRoundLoad = { viewModel.updateGame01State(Game01State.ROUND_LOAD) },
+        updateGame01StateToRoundReady = { viewModel.updateGame01State(Game01State.ROUND_READY) },
+        updateGame01StateToReview = { viewModel.updateGame01State(Game01State.REVIEW) },
+        getCurrentRound = { viewModel.getCurrentRound() },
+        increaseRound = { viewModel.increaseRound() },
+    )
 
     when (viewModel.game01State) {
         Game01State.INIT ->
@@ -60,6 +76,54 @@ fun Game01Screen(modifier: Modifier = Modifier) {
             Game01ResultScreen(
                 viewModel = viewModel,
             )
+    }
+}
+
+@Composable
+fun HandleGame01Event(
+    game01UiEvent: SharedFlow<Game01UiEvent>,
+    updateGame01StateToRoundLoad: () -> Unit,
+    updateGame01StateToRoundReady: () -> Unit,
+    updateGame01StateToReview: () -> Unit,
+    fetchQuizIdsInfo: () -> Unit,
+    fetchQuizData: () -> Unit,
+    fetchFinalResult: () -> Unit,
+    getCurrentRound: () -> Int,
+    increaseRound: () -> Unit,
+) {
+    LaunchedEffect(game01UiEvent) {
+        game01UiEvent.collectLatest { event ->
+            when (event) {
+                Game01UiEvent.CompleteUserInfoLoad -> {
+                    fetchQuizIdsInfo()
+                }
+
+                Game01UiEvent.CompleteQuizIdsLoad -> {
+                    updateGame01StateToRoundLoad()
+                    fetchQuizData()
+                }
+
+                Game01UiEvent.CompleteQuizLoad -> {
+                    updateGame01StateToRoundReady()
+                }
+
+                Game01UiEvent.CompletePostUserAnswer -> {
+                    val currentRound = getCurrentRound()
+
+                    if (currentRound == ROUND_COUNT) {
+                        fetchFinalResult()
+                    } else {
+                        increaseRound()
+                        fetchQuizData()
+                        updateGame01StateToRoundLoad()
+                    }
+                }
+
+                Game01UiEvent.CompleteGameResultLoad -> {
+                    updateGame01StateToReview()
+                }
+            }
+        }
     }
 }
 
