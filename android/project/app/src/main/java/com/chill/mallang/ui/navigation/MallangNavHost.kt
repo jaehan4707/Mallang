c0c957c5c7ba486186fc1c53a748a65f319d7f29
김성수 <skele1207@gmail.com>
2024-08-08 16:22:45 +0900

    refactor(#S11P12D203-280): 불필요한 파라미터 삭제

--- android/project/app/src/main/java/com/chill/mallang/ui/navigation/MallangNavHost.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/navigation/MallangNavHost.kt
@@ -141,7 +141,7 @@ fun MallangNavHost(
             arguments = DestinationAreaDetail.arguments,
         ) { navBackStackEntry ->
             val areaId = navBackStackEntry.arguments?.getLong(DestinationAreaDetail.arg)
-            FortDetailScreen(areaId = 1, userId = 1, teamId = 1)
+            FortDetailScreen(areaId = 1)
         }
 
         composable(
