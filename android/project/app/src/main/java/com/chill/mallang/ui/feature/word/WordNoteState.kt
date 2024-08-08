da8a1aed16968fca9af7a12faad3447798e8bf4e
NAKYUNG LEE <lnk12888@naver.com>
2024-08-08 13:39:47 +0900

    feat(#S11P12D203-230): WordNoteState interface로 변경

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/word/WordNoteState.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/word/WordNoteState.kt
@@ -1,5 +1,20 @@
 package com.chill.mallang.ui.feature.word
 
-data class WordNoteState(
-    val wordList: List<Word> = emptyList(),
-)
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
+
+@Stable
+sealed interface WordNoteState {
+    @Immutable
+    data object Loading : WordNoteState
+
+    @Immutable
+    data class Success(
+        val wordList: List<Word> = emptyList(),
+    ) : WordNoteState
+
+    @Immutable
+    data class Error(
+        val errorMessage: String = "",
+    ) : WordNoteState
+}
