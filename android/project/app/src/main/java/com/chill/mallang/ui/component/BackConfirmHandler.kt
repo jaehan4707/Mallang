ad2489b65d10e6bfe9bb1e09eb737b522add3dbf
이재한 <99114456+jaehan4707@users.noreply.github.com>
2024-08-08 14:20:17 +0900

    feat(#S11P12D203-291): BackConfirmHandler 파라미터 수정

--- android/project/app/src/main/java/com/chill/mallang/ui/component/BackConfirmHandler.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/component/BackConfirmHandler.kt
@@ -4,32 +4,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.res.stringResource
-import com.chill.mallang.R
 
 @Composable
 fun BackConfirmHandler(
     isBackPressed: Boolean = true,
-    onConfirm: () -> Unit,
-    onDismiss: (Boolean) -> Unit,
-    content: String = stringResource(R.string.confirm_dialog_default_message),
+    onConfirm: Pair<String, () -> Unit>,
+    onDismiss: Pair<String, (Boolean) -> Unit>,
+    title: String = "",
+    content: String = "",
 ) {
-    val showDialog = remember {
-        mutableStateOf(false)
-    }
+    val showDialog =
+        remember {
+            mutableStateOf(false)
+        }
     LaunchedEffect(isBackPressed) {
         showDialog.value = isBackPressed
     }
 
     if (showDialog.value) {
         BackConfirmDialog(
-            onConfirm = {
-                onConfirm()
-            },
-            onDismiss = {
-                onDismiss(false)
-            },
-            content = content
+            title = title,
+            onConfirm = onConfirm,
+            onDismiss = Pair(onDismiss.first) { onDismiss.second(false) },
+            content = content,
         )
     }
 }
