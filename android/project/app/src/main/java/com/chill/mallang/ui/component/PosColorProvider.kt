1c8d3ae574a8ccb6124439a0996163be6c428741
NAKYUNG LEE <lnk12888@naver.com>
2024-08-08 17:20:00 +0900

    feat(#S11P12D203-231): 주석 수정

--- android/project/app/src/main/java/com/chill/mallang/ui/component/PosColorProvider.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/component/PosColorProvider.kt
@@ -6,15 +6,15 @@ import com.chill.mallang.ui.theme.Gray6
 object PosColorProvider {
     private val colorMap =
         mapOf(
-            "명사" to Color(0xFFE57373), // Red shade for Noun
-            "대명사" to Color(0xFFBA68C8), // Purple shade for Pronoun
-            "수사" to Color(0xFF7986CB), // Blue shade for Numeral
-            "동사" to Color(0xFF64B5F6), // Light Blue shade for Verb
-            "형용사" to Color(0xFF4DB6AC), // Teal shade for Adjective
-            "관형사" to Color(0xFF81C784), // Green shade for Determiner
-            "부사" to Color(0xFFFFD54F), // Yellow shade for Adverb
-            "조사" to Color(0xFFFF8A65), // Orange shade for Postposition
-            "감탄사" to Color(0xFFA1887F), // Brown shade for Interjection
+            "명사" to Color(0xFFE57373), // 빨강
+            "대명사" to Color(0xFFBA68C8), // 보라
+            "수사" to Color(0xFF7986CB), // 파랑
+            "동사" to Color(0xFF64B5F6), // 하늘
+            "형용사" to Color(0xFF4DB6AC), // 민트
+            "관형사" to Color(0xFF81C784), // 초록
+            "부사" to Color(0xFFFFD54F), // 노랑
+            "조사" to Color(0xFFFF8A65), // 주황
+            "감탄사" to Color(0xFFA1887F), // 갈색
         )
 
     fun getColorForPos(pos: String): Color {
