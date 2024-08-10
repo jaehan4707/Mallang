db0e996732f6492c8e86a63aea626ab57f69d3a8
SeungSoo <skele1207@gmail.com>
2024-08-10 21:17:32 +0900

    feat(#S11P12D203): 버튼 클릭 시 애니메이션

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/home/layout/BottomButtonHolder.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/home/layout/BottomButtonHolder.kt
@@ -1,5 +1,8 @@
 package com.chill.mallang.ui.feature.home.layout
 
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,12 +13,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.chill.mallang.ui.theme.Gray2
 import com.chill.mallang.ui.theme.MallangTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 
 @Composable
 fun BottomButtonHolder(
@@ -24,13 +34,34 @@ fun BottomButtonHolder(
     onClickMap: () -> Unit = {},
     onClickSetting: () -> Unit = {},
 ) {
+    val eventFlow = remember { MutableStateFlow<ButtonEvent>(ButtonEvent.None) }
+    val event by eventFlow.collectAsState()
+
+    val height by animateIntAsState(
+        targetValue = if (event is ButtonEvent.None) 210 else 0,
+        animationSpec = tween(durationMillis = 300),
+        finishedListener = { _ ->
+            when (event) {
+                ButtonEvent.Map -> onClickMap()
+                ButtonEvent.Study -> onClickStudy()
+                else -> {}
+            }
+        },
+    )
+
     Box(
         modifier =
             modifier
                 .fillMaxWidth()
-                .height(210.dp)
+                .height(height.dp)
+                .clipToBounds()
+                .background(color = Color.White)
                 .border(width = 8.dp, color = Gray2)
-                .border(width = 8.dp, color = Gray2, shape = RoundedCornerShape(24.dp)),
+                .border(
+                    width = 8.dp,
+                    color = Gray2,
+                    shape = RoundedCornerShape(24.dp),
+                ),
     ) {
         SettingsButton(
             modifier =
@@ -46,10 +77,22 @@ fun BottomButtonHolder(
                     .align(Alignment.Center),
             horizontalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-            StudyButton(onClick = onClickStudy)
-            MapButton(onClick = onClickMap)
+            StudyButton(onClick = {
+                eventFlow.update { ButtonEvent.Study }
+            })
+            MapButton(onClick = {
+                eventFlow.update { ButtonEvent.Map }
+            })
         }
     }
+}
+
+sealed interface ButtonEvent {
+    data object None : ButtonEvent
+
+    data object Study : ButtonEvent
+
+    data object Map : ButtonEvent
 }
 
 @Preview(showBackground = true, showSystemUi = true)
