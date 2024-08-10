85c2012f4fbeb3e353592d6578884f95349a7974
nakyung128 <lnk12888@naver.com>
2024-08-10 13:50:49 +0900

    feat(#S11P12D203-227): 학습 문제 불러오는 api ViewModel에 구현

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/study/StudyViewModel.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/study/StudyViewModel.kt
@@ -3,54 +3,93 @@ package com.chill.mallang.ui.feature.study
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.chill.mallang.data.model.response.ApiResponse
+import com.chill.mallang.data.repository.local.DataStoreRepository
+import com.chill.mallang.data.repository.remote.StudyRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class StudyViewModel
     @Inject
-    constructor() : ViewModel() {
+    constructor(
+        val savedStateHandle: SavedStateHandle,
+        private val studyRepository: StudyRepository,
+        private val dataStoreRepository: DataStoreRepository,
+    ) : ViewModel() {
         private val _studyState = MutableStateFlow<StudyState>(StudyState.Loading)
         val studyState = _studyState.asStateFlow()
 
         var selectedAnswer by mutableIntStateOf(-1)
 
+        init {
+            loadQuizData(savedStateHandle.get<Long>("studyId") ?: -1)
+        }
+
         // 퀴즈 데이터 로드
-        fun loadQuizData(studyId: Int) {
+        fun loadQuizData(studyId: Long) {
             viewModelScope.launch {
-                if (studyId != -1) {
-                    // 풀었던 문제 api 호출 및 통신
-                    _studyState.value =
-                        StudyState.Success(
-                            quizTitle = "풀었던 문제입니당당",
-                            quizScript = "우리나라의 경제는 그동안 세계에 유례가 없을 정도로 __ 할만한 성장을 이루었다.",
-                            wordList =
-                                arrayListOf(
-                                    "괄목",
-                                    "상대",
-                                    "과장",
-                                    "시기",
-                                ),
-                        )
+                if (studyId != -1L) {
+                    dataStoreRepository.getUserId()?.let { userId ->
+                        studyRepository
+                            .getIncorrectQuiz(userId = userId, studyId = studyId)
+                            .collectLatest { response ->
+                                when (response) {
+                                    is ApiResponse.Success -> {
+                                        val data = response.body!!
+                                        _studyState.value =
+                                            StudyState.Success(
+                                                studyId = data.studyId,
+                                                quizTitle = data.quizTitle,
+                                                quizScript = data.quizScript,
+                                                wordList = data.wordList,
+                                            )
+                                    }
+
+                                    is ApiResponse.Error -> {
+                                        _studyState.value =
+                                            StudyState.Error(
+                                                errorMessage = response.errorMessage,
+                                            )
+                                    }
+
+                                    ApiResponse.Init -> {}
+                                }
+                            }
+                    }
                 } else {
-                    // 새로운 문제 api 호출 및 통신
-                    _studyState.value =
-                        StudyState.Success(
-                            quizTitle = "빈칸을 채워 주세요",
-                            quizScript = "우리나라의 경제는 그동안 세계에 유례가 없을 정도로 ___ 할만한 성장을 이루었다.",
-                            wordList =
-                                arrayListOf(
-                                    "괄목",
-                                    "상대",
-                                    "과장",
-                                    "시기",
-                                ),
-                        )
+                    dataStoreRepository.getUserId()?.let { userId ->
+                        studyRepository.getStudyQuiz(userId).collectLatest { response ->
+                            when (response) {
+                                is ApiResponse.Success -> {
+                                    val data = response.body!!
+                                    _studyState.value =
+                                        StudyState.Success(
+                                            studyId = data.studyId,
+                                            quizTitle = data.quizTitle,
+                                            quizScript = data.quizScript,
+                                            wordList = data.wordList,
+                                        )
+                                }
+
+                                is ApiResponse.Error -> {
+                                    _studyState.value =
+                                        StudyState.Error(
+                                            errorMessage = response.errorMessage,
+                                        )
+                                }
+
+                                ApiResponse.Init -> {}
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -59,7 +98,35 @@ class StudyViewModel
             selectedAnswer = index + 1
         }
 
-        fun submitQuiz() {
+        fun submitQuiz(
+            studyId: Long,
+            answer: Int,
+        ) {
             // 퀴즈 채점 api
+            viewModelScope.launch {
+                dataStoreRepository.getUserId()?.let { userId ->
+                    studyRepository
+                        .submitStudyAnswer(userId, studyId, answer)
+                        .collectLatest { response ->
+                            when (response) {
+                                is ApiResponse.Success -> {
+                                    _studyState.value =
+                                        StudyState.SubmitSuccess(
+                                            studyId = studyId
+                                        )
+                                }
+
+                                is ApiResponse.Error -> {
+                                    _studyState.value =
+                                        StudyState.Error(
+                                            errorMessage = response.errorMessage,
+                                        )
+                                }
+
+                                ApiResponse.Init -> {}
+                            }
+                        }
+                }
+            }
         }
     }
