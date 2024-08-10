7d9d4edd4edfdbe89d2de49f528e15ddf42fdbef
nakyung128 <lnk12888@naver.com>
2024-08-10 13:54:49 +0900

    feat(#S11P12D203-227): 학습 결과 불러오는 api 코드 추가

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/study_result/StudyResultViewModel.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/study_result/StudyResultViewModel.kt
@@ -1,11 +1,16 @@
 package com.chill.mallang.ui.feature.study_result
 
+import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.chill.mallang.data.model.response.ApiResponse
+import com.chill.mallang.data.repository.local.DataStoreRepository
+import com.chill.mallang.data.repository.remote.StudyRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -13,32 +18,47 @@ import javax.inject.Inject
 class StudyResultViewModel
     @Inject
     constructor(
-        savedStandHandle: SavedStateHandle,
+        savedStateHandle: SavedStateHandle,
+        private val studyRepository: StudyRepository,
+        private val dataStoreRepository: DataStoreRepository,
     ) : ViewModel() {
         private val _studyResultState = MutableStateFlow<StudyResultState>(StudyResultState.Loading)
         val studyResultState = _studyResultState.asStateFlow()
 
         init {
-            gradeQuiz(savedStandHandle.get<Int>("studyId") ?: -1)
+            Log.d("nakyung", savedStateHandle.get<Long>("studyId").toString())
+            gradeQuiz(savedStateHandle.get<Long>("studyId") ?: -1)
         }
 
-        private fun gradeQuiz(studyId: Int) {
+        private fun gradeQuiz(studyId: Long) {
+            // 퀴즈 결과 불러오기 api 실행
             viewModelScope.launch {
-                // 퀴즈 결과 불러오기 api 실행
-                _studyResultState.value =
-                    StudyResultState.Success(
-                        quizTitle = "빈칸을 채워 주세요",
-                        quizScript = "우리나라의 경제는 그동안 세계에 유례가 없을 정도로 __할만한 성장을 이루었다.",
-                        wordList =
-                            arrayListOf(
-                                "괄목" to "눈을 비비고 볼 정도로 매우 놀라다.",
-                                "상대" to "서로 마주 대하다.",
-                                "과장" to "사실보다 지나치게 불려서 말하거나 표현하다.",
-                                "시기" to "때나 경우.",
-                            ),
-                        result = false,
-                        systemAnswer = 1,
-                    )
+                dataStoreRepository.getUserId()?.let { userId ->
+                    studyRepository
+                        .getStudyQuizResult(userId = userId, studyId = studyId)
+                        .collectLatest { response ->
+                            when (response) {
+                                is ApiResponse.Success -> {
+                                    val data = response.body!!
+                                    _studyResultState.value =
+                                        StudyResultState.Success(
+                                            quizTitle = data.quizTitle,
+                                            quizScript = data.quizScript,
+                                            wordList = data.wordList,
+                                        )
+                                }
+
+                                is ApiResponse.Error -> {
+                                    _studyResultState.value =
+                                        StudyResultState.Error(
+                                            errorMessage = response.errorMessage,
+                                        )
+                                }
+
+                                ApiResponse.Init -> {}
+                            }
+                        }
+                }
             }
         }
     }
