efab5c099fc22bb3d85696f840bc55ede2b2f1d8
SeungSoo <skele1207@gmail.com>
2024-08-11 20:00:27 +0900

    feat(#S11P12D203-305): 점령팀에 따른 마커 변경

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/map/CustomMarkerState.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/map/CustomMarkerState.kt
@@ -3,10 +3,9 @@ package com.chill.mallang.ui.feature.map
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.setValue
 import com.chill.mallang.data.model.entity.Area
-import com.chill.mallang.ui.theme.Red01
 import com.google.maps.android.compose.MarkerState
 
 class CustomMarkerState(
@@ -16,7 +15,7 @@ class CustomMarkerState(
 
     var distance by mutableIntStateOf(0)
 
-    var color by mutableStateOf(Red01)
+    var occupyingTeamId by mutableLongStateOf(1L)
 
     var radius by mutableDoubleStateOf(10.0)
 }
