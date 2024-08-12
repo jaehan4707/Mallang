66136f0690c07a1c103267213c6602190334a019
이재한 <99114456+jaehan4707@users.noreply.github.com>
2024-08-12 13:00:40 +0900

    feat(#S11P12D203-328): SummaryUiState Success, Error 추가

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/summary/SummaryUiState.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/summary/SummaryUiState.kt
@@ -2,6 +2,9 @@ package com.chill.mallang.ui.feature.summary
 
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
+import com.chill.mallang.data.model.entity.Summary
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.persistentListOf
 
 @Stable
 sealed interface SummaryUiState {
@@ -9,5 +12,13 @@ sealed interface SummaryUiState {
     data object Loading : SummaryUiState
 
     @Immutable
-    data object Success : SummaryUiState
+    data class Success(
+        val summaryRecords: PersistentList<Summary> = persistentListOf(),
+    ) : SummaryUiState
+
+    @Immutable
+    data class Error(
+        val errorCode: Int = -1,
+        val errorMessage: String = "",
+    ) : SummaryUiState
 }
