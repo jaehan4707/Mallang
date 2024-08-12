7551bb8714f4a9084711c8a7c9eabe91857fdfd6
강승우 <pooya98@naver.com>
2024-08-12 12:58:22 +0900

    ui(#S11P12D203-262): 게임 스플래시 화면 UI 구현

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/game/game01/SubView/Game01SplashScreen.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/game/game01/SubView/Game01SplashScreen.kt
@@ -3,13 +3,23 @@ package com.chill.mallang.ui.feature.game.game01.SubView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.chill.mallang.R
 import com.chill.mallang.ui.theme.MallangTheme
 import com.chill.mallang.ui.theme.Typography
@@ -18,20 +28,46 @@ import com.chill.mallang.ui.theme.Typography
 fun Game01SplashScreen(
     modifier: Modifier = Modifier,
 ) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.game_splash_background))
+    val progress by animateLottieCompositionAsState(
+        composition,
+        iterations = LottieConstants.IterateForever,
+    )
+
     Box(
         modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center,
     ) {
+        LottieAnimation(
+            composition = composition,
+            progress = progress,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillBounds
+        )
         Image(
             painter = painterResource(id = R.drawable.img_game_splash),
             contentDescription = "",
-            modifier = modifier.fillMaxSize(),
+            modifier = modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 30.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.img_game_splash_title),
+            contentDescription = "",
+            modifier = modifier
+                .fillMaxWidth()
+                .align(Alignment.TopCenter)
+                .padding(top = 20.dp)
+                .padding(horizontal = 20.dp),
         )
         Text(
             text = "Loading...",
             style = Typography.displayLarge,
             fontSize = 30.sp,
-            modifier = modifier.align(Alignment.BottomCenter),
+            modifier = modifier
+                .align(Alignment.BottomCenter)
+                .padding(20.dp),
         )
     }
 }
