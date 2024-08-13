63d0cb7fd107fe4b1bbb32d2fef8c583beec7a57
강승우 <pooya98@naver.com>
2024-08-13 16:17:12 +0900

    ui(#S11P12D203-362): 게임 보상 화면 UI 구현 및 데이터 연결

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/game/game01/SubView/RewardScreen/Game01RewardScreen.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/game/game01/SubView/RewardScreen/Game01RewardScreen.kt
@@ -3,8 +3,6 @@ package com.chill.mallang.ui.feature.game.game01.SubView.RewardScreen
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -23,9 +21,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,18 +33,33 @@ import com.airbnb.lottie.compose.rememberLottieComposition
 import com.chill.mallang.R
 import com.chill.mallang.ui.component.LongBlackButton
 import com.chill.mallang.ui.component.experiencebar.AnimatedGaugeBar
+import com.chill.mallang.ui.component.experiencebar.BigBadge
 import com.chill.mallang.ui.theme.MallangTheme
 import com.chill.mallang.ui.theme.Typography
 import kotlinx.coroutines.delay
 
 @Composable
 fun Game01RewardScreen(
+    userExp: Float = 0F,
+    userGameScore: Float = 0F,
+    userLevel: Int = 1,
+    LevelExp: Float = 100F,
     completeReward: () -> Unit = {},
     modifier: Modifier = Modifier,
 ) {
-    var isBadgeSlide by remember { mutableStateOf(false) }
-    var isBadgeTransparent by remember { mutableStateOf(false) }
+    // 인장 애니메이션 관련 변수
+    var isBadgeSlide by remember { mutableStateOf(true) }
+    var isBadgeTransparent by remember { mutableStateOf(true) }
     var isMessageTransparent by remember { mutableStateOf(false) }
+    var isFanFareTransparent by remember { mutableStateOf(false) }
+
+    // 경험치 애니메이션 관련 변수
+    var initalExpRate by remember { mutableStateOf(userExp / LevelExp) }
+    var targetExpRate by remember { mutableStateOf(userExp / LevelExp) }
+    var currentUserLevel by remember { mutableStateOf(userLevel) }
+    var showingUserLevel by remember { mutableStateOf(userLevel) }
+    var nextUserLevel by remember { mutableStateOf(userLevel + 1) }
+    var isLevelUp by remember { mutableStateOf(false) }
 
     val badgePaddingSize by animateDpAsState(
         targetValue = if (isBadgeSlide) 0.dp else 100.dp,
@@ -65,36 +76,60 @@ fun Game01RewardScreen(
         animationSpec = tween(durationMillis = 1000),
     )
 
+    val fanfareAlpha by animateFloatAsState(
+        targetValue = if (isFanFareTransparent) 1.0f else 0.0f,
+        animationSpec = tween(durationMillis = 1000),
+    )
+
     val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.celebration))
     val animationState = rememberLottieAnimatable()
 
-    LaunchedEffect(composition) {
-        delay(2000)
-        animationState.animate(
-            composition = composition,
-            iterations = 1,
-        )
+    if (userExp + userGameScore >= LevelExp) {
+        targetExpRate = 1F
+        isLevelUp = true
+    } else {
+        targetExpRate = (userExp + userGameScore) / LevelExp
     }
 
-    LaunchedEffect(Unit) {
-        delay(2000L)
-        isBadgeSlide = true
-        isBadgeTransparent = true
-        delay(1500L)
-        isMessageTransparent = true
+    LaunchedEffect(isLevelUp) {
+        if(isLevelUp) {
+            delay(2000L)
+            isBadgeTransparent = false
+            isBadgeSlide = false
+            currentUserLevel += 1
+            nextUserLevel += 1
+            targetExpRate = 0F
+            initalExpRate = 0F
+            delay(1000L)
+            isBadgeTransparent = true
+            isBadgeSlide = true
+            isFanFareTransparent = true
+            showingUserLevel += 1
+            val newRate = (userExp + userGameScore - LevelExp) / (LevelExp + 200)
+            targetExpRate = newRate
+            initalExpRate = newRate
+
+            animationState.animate(
+                composition = composition,
+                iterations = 1,
+            )
+
+            delay(1000L)
+            isMessageTransparent = true
+        }
     }
 
     Box(
         modifier =
-            modifier
-                .fillMaxSize()
-                .padding(vertical = 20.dp),
+        modifier
+            .fillMaxSize()
+            .padding(vertical = 20.dp),
     ) {
         Column(
             modifier =
-                modifier
-                    .fillMaxSize()
-                    .align(Alignment.Center),
+            modifier
+                .fillMaxSize()
+                .align(Alignment.Center),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Text(
@@ -102,48 +137,39 @@ fun Game01RewardScreen(
                 style = Typography.displayLarge,
                 fontSize = 40.sp,
                 modifier =
-                    modifier
-                        .padding(top = 100.dp)
-                        .graphicsLayer(alpha = messageAlpha),
+                modifier
+                    .padding(top = 100.dp)
+                    .graphicsLayer(alpha = messageAlpha),
             )
             Box(
                 modifier =
                     Modifier
-                        .size(350.dp),
+                        .size(400.dp),
                 contentAlignment = Alignment.Center,
             ) {
                 LottieAnimation(
                     composition = composition,
                     progress = animationState.progress,
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .graphicsLayer(alpha = fanfareAlpha),
                 )
-                Box(modifier = Modifier.size(100.dp).background(Color.White))
-                Image(
+                BigBadge(
+                    level = showingUserLevel,
                     modifier =
-                        Modifier
-                            .size(250.dp)
-                            .padding(top = badgePaddingSize)
-                            .align(Alignment.Center)
-                            .graphicsLayer(alpha = badgeAlpha),
-                    painter =
-                        painterResource(id = R.drawable.img_badge_5),
-                    contentDescription = "",
+                    Modifier
+                        .padding(top = badgePaddingSize)
+                        .graphicsLayer(alpha = badgeAlpha),
                 )
             }
-            Text(
-                "골드 1",
-                style = Typography.titleLarge,
-                fontSize = 30.sp,
-                modifier = modifier.graphicsLayer(alpha = badgeAlpha),
-            )
             Spacer(modifier = Modifier.size(80.dp))
         }
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier =
-                modifier
-                    .padding(horizontal = 40.dp, vertical = 80.dp)
-                    .align(Alignment.BottomCenter),
+            modifier
+                .padding(horizontal = 40.dp, vertical = 80.dp)
+                .align(Alignment.BottomCenter),
         ) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
@@ -151,21 +177,21 @@ fun Game01RewardScreen(
                 verticalAlignment = Alignment.CenterVertically,
             ) {
                 Text(
-                    text = "21",
+                    text = "$currentUserLevel",
                     style = Typography.displayLarge,
                     fontSize = 40.sp,
                 )
-                Text("EXP+135")
+                Text("EXP +${userGameScore.toInt()}")
                 Text(
-                    text = "22",
+                    text = "$nextUserLevel",
                     style = Typography.displayLarge,
                     fontSize = 40.sp,
                 )
             }
             AnimatedGaugeBar(
                 modifier = modifier.padding(top = 10.dp),
-                initialValue = 0f,
-                targetValue = 0.7f,
+                initialValue = initalExpRate,
+                targetValue = targetExpRate,
             )
         }
         LongBlackButton(
@@ -180,6 +206,12 @@ fun Game01RewardScreen(
 @Composable
 fun Game01RewardScreenPreview() {
     MallangTheme {
-        Game01RewardScreen()
+        Game01RewardScreen(
+            userExp = 100F,
+            userGameScore = 135F,
+            userLevel = 10,
+            LevelExp = 200F,
+            completeReward = {},
+        )
     }
 }
