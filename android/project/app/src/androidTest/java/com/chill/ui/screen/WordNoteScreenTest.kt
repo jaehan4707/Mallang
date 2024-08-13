3d067630e06235ac2a794afdfa419fdf897a841d
jaehan4707 <jahan4707@naver.com>
2024-08-14 00:17:10 +0900

    test(#S11P12D203-345): WordNoteScreenTest 불러온 단어장이 화면에 표시되는지 검증 추가

---
+++ android/project/app/src/androidTest/java/com/chill/ui/screen/WordNoteScreenTest.kt
@@ -0,0 +1,53 @@
+package com.chill.ui.screen
+
+import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onChildren
+import androidx.compose.ui.test.onNodeWithTag
+import com.chill.data.WordNoteTestData.wordList
+import com.chill.mallang.data.model.response.ApiResponse
+import com.chill.mallang.data.repository.local.DataStoreRepository
+import com.chill.mallang.data.repository.remote.StudyRepository
+import com.chill.mallang.ui.feature.word.HandleWordNoteUi
+import com.chill.mallang.ui.feature.word.WordNoteViewModel
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class WordNoteScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private lateinit var viewModel: WordNoteViewModel
+    private val studyRepository: StudyRepository = mockk()
+    private val dataStoreRepository: DataStoreRepository = mockk()
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(Dispatchers.Unconfined)
+        coEvery { dataStoreRepository.getUserId() } returns 1L
+    }
+
+    @Test
+    fun `불러온_단어장들은_화면에_표시된다`() = runTest {
+        val userId = dataStoreRepository.getUserId() ?: 1L
+        coEvery { studyRepository.getWordList(userId) } returns flowOf(
+            ApiResponse.Success(wordList)
+        )
+        viewModel = WordNoteViewModel(studyRepository, dataStoreRepository)
+        composeTestRule.setContent {
+            HandleWordNoteUi(uiState = viewModel.wordNoteState.value)
+        }
+        composeTestRule.onNodeWithTag("word_note_list").onChildren()
+            .assertCountEquals(wordList.size)
+    }
+}
