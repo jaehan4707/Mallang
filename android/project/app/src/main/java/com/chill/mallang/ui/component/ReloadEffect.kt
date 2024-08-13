e7fe01b04b9e90f5031c000520907b27264b219c
김성수 <skele1207@gmail.com>
2024-08-13 14:37:39 +0900

    feat(#S11P12D203-318): 화면 진입 시 재 로딩 할 수 있는 Effect 추가

---
+++ android/project/app/src/main/java/com/chill/mallang/ui/component/ReloadEffect.kt
@@ -0,0 +1,25 @@
+package com.chill.mallang.ui.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@Composable
+fun ReloadEffect(
+    onLoad: () -> Unit = {},
+    onUnload: () -> Unit = {},
+) {
+    var isLoaded by remember { mutableStateOf(false) }
+
+    DisposableEffect(isLoaded) {
+        onLoad()
+        isLoaded = true
+        onDispose {
+            onUnload()
+            isLoaded = false
+        }
+    }
+}
