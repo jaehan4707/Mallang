a78a6cd9090edd1e4ba3baba700bc5c83ac804dd
이재한 <99114456+jaehan4707@users.noreply.github.com>
2024-08-14 15:32:44 +0900

    fix(#S11P12D203-290): 홈 상단바 유지 버그 해결

--- android/project/app/src/main/java/com/chill/mallang/ui/feature/home/HomeScreen.kt
+++ android/project/app/src/main/java/com/chill/mallang/ui/feature/home/HomeScreen.kt
@@ -46,6 +46,7 @@ import com.chill.mallang.ui.feature.home.layout.BottomButtonHolder
 import com.chill.mallang.ui.feature.home.layout.HomeBackground
 import com.chill.mallang.ui.feature.setting.EditNickNameDialogScreen
 import com.chill.mallang.ui.feature.setting.SettingDialog
+import com.chill.mallang.ui.feature.topbar.TopbarHandler
 import com.chill.mallang.ui.theme.Gray2
 import com.chill.mallang.ui.theme.MallangTheme
 import com.chill.mallang.ui.theme.Sub1
@@ -62,7 +63,7 @@ fun HomeScreen(
     popUpBackStack: () -> Unit = {},
     onShowErrorSnackBar: (String) -> Unit = {},
     exitApplication: () -> Unit = {},
-    viewModel: HomeViewModel = hiltViewModel()
+    viewModel: HomeViewModel = hiltViewModel(),
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
@@ -80,7 +81,7 @@ fun HomeScreen(
     )
 
     // TopBar
-    //TopbarHandler()
+    TopbarHandler()
 
     HandleHomeUiEvent(
         event = viewModel.event,
